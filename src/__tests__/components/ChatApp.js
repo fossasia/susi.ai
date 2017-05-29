e3e13947f289c72a460a47bc07ccf8101248b4fe
isuruAb <vpisabeywardana@gmail.com>
2017-05-29 13:45:10 +0530

    Fixes #81 add test cases

---
+++ src/__tests__/components/ChatApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChatApp from '../../components/ChatApp.react';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ChatApp />, div);
+});
