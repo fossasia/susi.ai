96567d38bd3f7af606786f4fcda3251dfbfa1294
Madhav Rathi <mdhvrthi@gmail.com>
2017-06-16 01:14:00 +0530

    Fixes #268: susi message search now works

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // imports
+<<<<<<< ad40ffcf71a4ecfa576a16f7e4d7938d5b5069ec
 import {  getLocation, createSUSIMessage } from './API.actions';
 import {  getHistory } from './History.actions';
 import {  createMessage,
@@ -10,6 +11,45 @@ import {  getDefaults,
           setDefaultTheme,
           setDefaultServer,
           ToggleSearch,
+=======
+import { getLocation, createSUSIMessage } from './API.actions';
+import { getHistory } from './History.actions';
+import { createMessage,
+  receiveCreatedMessage,
+  clickThread,
+  receiveAll } from './ChatApp.actions';
+import { getDefaults,
+         setDefaults,
+         setDefault// imports
+import { getLocation, createSUSIMessage } from './API.actions';
+import { getHistory } from './History.actions';
+import { createMessage,
+  receiveCreatedMessage,
+  clickThread,
+  receiveAll } from './ChatApp.actions';
+import { getDefaults,
+         setDefaults,
+         setDefaultTheme,
+         ToggleSearch,
+         themeChanged} from './Settings.actions';
+
+// exports
+export { getLocation, createSUSIMessage }
+export { getHistory }
+
+export {  createMessage,
+          receiveCreatedMessage,
+          clickThread,
+          receiveAll }
+
+export { getDefaults,
+  setDefaults,
+  setDefaultTheme,
+  ToggleSearch,
+  themeChanged }
+Theme,
+       ToggleSearch,
+>>>>>>> Fixes #268: susi message search now works
           themeChanged} from './Settings.actions';
 
 // exports
@@ -22,8 +62,15 @@ export {  createMessage,
           receiveAll }
 
 export { getDefaults,
+<<<<<<< ad40ffcf71a4ecfa576a16f7e4d7938d5b5069ec
          setDefaults,
          setDefaultTheme,
          setDefaultServer,
          ToggleSearch,
          themeChanged }
+=======
+  setDefaults,
+  setDefaultTheme,
+  ToggleSearch,
+  themeChanged }
+>>>>>>> Fixes #268: susi message search now works
