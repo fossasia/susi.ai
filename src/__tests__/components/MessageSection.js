4f6795cddeaa593d066d45510066eb10d0a826f3
Madhav Rathi <mdhvrthi@gmail.com>
2017-06-27 16:54:17 +0530

    fixes failures of test

--- src/__tests__/components/MessageSection.js
+++ src/__tests__/components/MessageSection.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import MessageSection from '../../components/ChatApp/MessageSection.react';
+import MessageSection from '../../components/ChatApp/MessageSection/MessageSection.react';
 import { shallow } from 'enzyme';
 
 it('render MessageListItem without crashing', () => {
   shallow(<MessageSection />);
 })
-
