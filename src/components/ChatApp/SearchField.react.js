74746055e347913b00ea4af1af7ae34799e6594b
Uday Theja <udayteja96@gmail.com>
2017-06-27 16:23:51 +0530

    Fixes #285 - Search Field Animated and App Bar Fixed (#332)
    * Fixes #285  (WIP) - Animation for search field
    
    Fixes #285 - Search field animation
    
    Fixes #285 - Search Field Animated and App Bar Fixed
    
    * Fixes #285 - Fixed overflow in mobile view
    
    Fixes #285 - Improved the animation for search field
    
    Fixes #285 - Improved search field animation

---
+++ src/components/ChatApp/SearchField.react.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {TextField, IconButton} from 'material-ui'
+import SearchIcon from 'material-ui/svg-icons/action/search';
+import UpIcon from 'material-ui/svg-icons/navigation/arrow-upward';
+import DownIcon from 'material-ui/svg-icons/navigation/arrow-downward';
+import ExitIcon from 'material-ui/svg-icons/navigation/close';
+
+const animationStyle = {
+    transition: 'width 0.75s cubic-bezier(0.000, 0.795, 0.000, 1.000)'
+};
+
+const additionalStyles={
+    text: animationStyle,
+    frame: animationStyle
+}
+
+class ExpandingSearchField extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {isOpen: false};
+    }
+
+    onClick = () => {
+        this.setState({isOpen: !this.state.isOpen});
+        if(!this.state.isOpen){
+            this.props.activateSearch();
+        }
+        else{
+            this.props.exitSearch();
+        }
+    }
+
+    onChange = (event) => {
+        this.props.onTextChange(event);
+    }
+
+    onClickRecent = () => {
+        this.props.scrollRecent();
+    }
+
+    onClickPrev = () => {
+        this.props.scrollPrev();
+    }
+
+    render() {
+
+    const baseStyles = {
+        open: {
+            width: 150,
+        },
+        closed: {
+            width: 0,
+        },
+        smallIcon: {
+            fill: 'white'
+        },
+        icon: {
+            width: 40,
+            height: 40,
+            padding: 5,
+            top: 10
+        },
+        frame: {
+        }
+    };
+
+    const searchStyle = {
+      WebkitTextFillColor: 'white',
+      color: 'white'
+    }
+
+    let textStyle = this.state.isOpen ? baseStyles.open : baseStyles.closed;
+    textStyle = Object.assign(textStyle,
+                        additionalStyles ? additionalStyles.text : {});
+
+    const divStyle = Object.assign({},textStyle, baseStyles.frame,
+                        additionalStyles ? additionalStyles.frame : {});
+    divStyle.width += baseStyles.icon.width + 5;
+    divStyle.display =  'inline';
+
+    if(this.state.isOpen){
+        return (
+            <div style={divStyle}>
+                <IconButton
+                    iconStyle={baseStyles.smallIcon}
+                    style={baseStyles.icon}
+                    onClick={this.onClick}>
+                    <SearchIcon />
+                </IconButton>
+                <TextField
+                    name='search'
+                    className='search'
+                    hintText="Search..."
+                    inputStyle={searchStyle}
+                    style={textStyle}
+                    onChange={(event) => this.onChange(event)}
+                    autoFocus/>
+                <IconButton
+                    iconStyle={baseStyles.smallIcon}
+                    style={baseStyles.icon}
+                    onClick={this.onClickPrev}>
+                    <UpIcon />
+                </IconButton>
+                <IconButton
+                    iconStyle={baseStyles.smallIcon}
+                    style={baseStyles.icon}
+                    onClick={this.onClickRecent}>
+                    <DownIcon />
+                </IconButton>
+                <IconButton
+                    iconStyle={baseStyles.smallIcon}
+                    style={baseStyles.icon}
+                    onClick={this.onClick}>
+                    <ExitIcon />
+                </IconButton>
+            </div>
+        );
+    }
+    return (
+            <div style={divStyle}>
+                <IconButton
+                    iconStyle={baseStyles.smallIcon}
+                    style={baseStyles.icon}
+                    onClick={this.onClick}>
+                    <SearchIcon />
+                </IconButton>
+                <TextField
+                    name='search'
+                    style={textStyle}
+                    inputStyle={searchStyle} />
+            </div>
+        );
+    }
+};
+
+ExpandingSearchField.propTypes = {
+    activateSearch: PropTypes.func,
+    exitSearch: PropTypes.func,
+    onTextChange: PropTypes.func,
+    scrollRecent: PropTypes.func,
+    scrollPrev: PropTypes.func,
+};
+
+export default ExpandingSearchField;
