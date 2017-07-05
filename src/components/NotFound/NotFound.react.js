247f100f0d3f77d0db19f7de9a6c00d8d60537a5
Rishi Raj <rishiraj824@gmail.com>
2017-07-05 12:04:46 +0530

    Fixes #367: Reuse the Login.react in Auth folder for SignUp component

--- src/components/NotFound/NotFound.react.js
+++ src/components/NotFound/NotFound.react.js
@@ -4,14 +4,18 @@ import { Link } from 'react-router-dom';
 import './NotFound.css';
 import LogoImg from '../images/susi.svg';
 import UserPreferencesStore from '../../stores/UserPreferencesStore';
-import LoginDialog from '../ChatApp/Dialog/LoginDialog';
+import Login from '../Auth/Login/Login.react';
+import SignUp from '../Auth/SignUp/SignUp.react';
+import Dialog from 'material-ui/Dialog';
+
 
 export default class NotFound extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            open: false
+            open: false,
+            loginOpen: false
         }
     }
     handleOpen = () => {
@@ -20,10 +24,36 @@ export default class NotFound extends Component {
     handleClose = () => {
         this.setState({ open: false });
     };
-      switchDialog = (dialogState) => {
-    this.setState({ open: dialogState });
-  };
+    handleLoginOpen = () => {
+        this.setState({
+            loginOpen: true
+        })
+    }
+    handleLoginClose = () => {
+        this.setState({
+            loginOpen: false
+        })
+    }
+
     render() {
+        const loginActions = <RaisedButton
+          label="Cancel"
+          backgroundColor={
+            UserPreferencesStore.getTheme()==='light' ? '#607D8B' : '#19314B'}
+          labelColor="#fff"
+          width='200px'
+          keyboardFocused={true}
+          onTouchTap={this.handleLoginClose}
+        />;
+        const signUpActions = <RaisedButton
+          label="Cancel"
+          backgroundColor={
+            UserPreferencesStore.getTheme()==='light' ? '#607D8B' : '#19314B'}
+          labelColor="#fff"
+          width='200px'
+          keyboardFocused={true}
+          onTouchTap={this.handleClose}
+        />;
 
         return (
             <div>
@@ -48,28 +78,42 @@ export default class NotFound extends Component {
                                 labelColor='#fff'
                             />
                         </Link>
-                        <Link to={'/signup'} className='actionButton'>
-                            <RaisedButton
+                        <RaisedButton
                                 className='notfound-button'
                                 label='SignUp to SUSI'
+                                onTouchTap={this.handleOpen}
                                 backgroundColor={
                                 UserPreferencesStore.getTheme() ? '#607D8B' : '#19314B'}
                                 labelColor='#fff'
-                            />
-                        </Link>
+                        />
                         <RaisedButton
                             className='notfound-button'
                             label='SignIn to SUSI'
-                            onTouchTap={this.handleOpen}
+                            onTouchTap={this.handleLoginOpen}
                             backgroundColor={
                                 UserPreferencesStore.getTheme() ? '#607D8B' : '#19314B'}
                             labelColor='#fff'
                         />
                     </div>
                 </div>
-            <LoginDialog {...this.props}
-            open={this.state.open}
-            switchDialog={this.switchDialog} />
+                <Dialog
+                  className='dialogStyle'
+                  actions={loginActions}
+                  modal={false}
+                  open={this.state.loginOpen}
+                  autoScrollBodyContent={true}
+                  contentStyle={{width: '35%',minWidth: '300px'}}>
+                  <Login {...this.props} />
+                </Dialog>
+                <Dialog
+                  className='dialogStyle'
+                  actions={signUpActions}
+                  modal={false}
+                  open={this.state.open}
+                  autoScrollBodyContent={true}
+                  contentStyle={{width: '35%',minWidth: '300px'}}>
+                  <SignUp {...this.props} />
+                </Dialog>
 
             </div>
 
