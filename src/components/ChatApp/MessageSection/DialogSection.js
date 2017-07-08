143dff5bd08d13db00b3b7253e024c4b70251e69
Madhav Rathi <mdhvrthi@gmail.com>
2017-07-08 17:36:09 +0530

    Fixes#416: Added done button in custom theme dialog in place of cancel button (#417)
    * Fixes#416: Added done button in custom theme dialog in place of cancel button
    
    * Added camel case for customSettingsDone

--- src/components/ChatApp/MessageSection/DialogSection.js
+++ src/components/ChatApp/MessageSection/DialogSection.js
@@ -83,7 +83,7 @@ export default class DialogSection extends Component {
         </Dialog>
       {/* ThemeChanger */}
         <Dialog
-          actions={this.props.actions}
+          actions={this.props.customSettingsDone}
           modal={false}
           open={this.props.openThemeChanger}
           autoScrollBodyContent={true}
@@ -109,6 +109,7 @@ DialogSection.propTypes = {
     openThemeChanger: PropTypes.bool,
     ServerChangeActions: PropTypes.array,
     HardwareActions: PropTypes.array,
+    customSettingsDone: PropTypes.object,
     ThemeChangerComponents: PropTypes.array,
     actions: PropTypes.object,
     bodyStyle: PropTypes.object,
