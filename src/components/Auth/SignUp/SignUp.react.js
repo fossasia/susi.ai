bfd7f6ca41fe95b7b5712143585c6d7c0071a614
Isuru Abeywardana <vpisabeywardana@gmail.com>
2017-07-19 18:12:45 +0530

    Fixes: #462 (partially) Add checkbox instead of radio button group (#483)
    * Fixes: #462 (partially) Add checkbox instead of radiobutton group
    
    minor change
    
    add standerd url to server field
    
    add toggle
    
    Fixes: #501 - Options Popover close after selecting a option
    
    align to right place
    
    minor change
    
    minor change remove extra codes
    
    alignment changes
    
    minor changes
    
    minor changes
    
    chage width
    
    width changed
    
    minor change
    
    * fix the little jump
    
    * did changes according to the comments
    
    * space changed

--- src/components/Auth/SignUp/SignUp.react.js
+++ src/components/Auth/SignUp/SignUp.react.js
@@ -8,10 +8,10 @@ import PasswordField from 'material-ui-password-field';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import PropTypes from 'prop-types';
-import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import UserPreferencesStore from '../../../stores/UserPreferencesStore';
 import Login from '../../Auth/Login/Login.react';
 import zxcvbn from 'zxcvbn';
+import Toggle from 'material-ui/Toggle';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -76,12 +76,43 @@ export default class SignUp extends Component {
             });
         }
     }
+    handleServeChange=(event)=>{
+        let state = this.state;
+        let serverUrl
+        if (event.target.value === 'customServer') {
+            state.checked?state.checked=false:state.checked=true;
+
+            if(state.checked){
+                let defaults = UserPreferencesStore.getPreferences();
+                state.serverUrl=defaults.Server
+                state.serverFieldError = false;
+            }
+
+        }
+        else if (event.target.name === 'serverUrl'){
+            serverUrl = event.target.value;
+            let validServerUrl =
+/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:~+#-]*[\w@?^=%&amp;~+#-])?/i
+            .test(serverUrl);
+            state.serverUrl = serverUrl;
+            state.serverFieldError = !(serverUrl && validServerUrl);
+        }
+                        this.setState(state);
+
+        if (this.state.serverFieldError) {
+            this.customServerMessage = 'Enter a valid URL';
+        }
+        else{
+            this.customServerMessage = '';
+        }
+
+    }
 
     handleChange = (event) => {
         let email;
         let password;
         let confirmPassword;
-        let serverUrl;
+        // let serverUrl;
         let state = this.state
         if (event.target.name === 'email') {
             email = event.target.value.trim();
@@ -89,7 +120,7 @@ export default class SignUp extends Component {
                 /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
             state.email = email;
             state.isEmail = validEmail;
-            state.emailError = !(email && validEmail)
+            state.emailError = !(email || validEmail)
         }
         else if (event.target.name === 'password') {
             password = event.target.value;
@@ -119,22 +150,6 @@ export default class SignUp extends Component {
             let validPassword = confirmPassword === password;
             state.confirmPasswordValue = confirmPassword;
             state.passwordConfirmError = !(validPassword && confirmPassword);
-        }
-        else if (event.target.value === 'customServer') {
-            state.checked = true;
-            state.serverFieldError = true;
-        }
-        else if (event.target.value === 'standardServer') {
-            state.checked = false;
-            state.serverFieldError = false;
-        }
-        else if (event.target.name === 'serverUrl'){
-            serverUrl = event.target.value;
-            let validServerUrl =
-/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:~+#-]*[\w@?^=%&amp;~+#-])?/i
-            .test(serverUrl);
-            state.serverUrl = serverUrl;
-            state.serverFieldError = !(serverUrl && validServerUrl);
         }
 
         if (!this.state.emailError
@@ -169,12 +184,12 @@ export default class SignUp extends Component {
             this.passwordConfirmErrorMessage = '';
         }
 
-        if (this.state.serverFieldError) {
+/*         if (this.state.serverFieldError) {
             this.customServerMessage = 'Enter a valid URL';
         }
         else{
             this.customServerMessage = '';
-        }
+        } */
 
         if(this.state.emailError||
         this.state.passwordError||
@@ -280,23 +295,22 @@ export default class SignUp extends Component {
             });
     }
     render() {
-
-        const serverURL = <TextField name="serverUrl"
-                            onChange={this.handleChange}
+        const customUrlStyle= {
+            width:'175px',
+            textAlign:'left',
+            margin:'-35px 0 0px 30px',
+        }
+        const serverURL = <TextField
+                            name="serverUrl"
+                            className="serverUrl"
+                            onChange={this.handleServeChange}
+                            onTouchTap={this.handleServeChange}
                             value={this.state.serverUrl}
                             errorText={this.customServerMessage}
-                            floatingLabelText="Custom URL" />;
+                            floatingLabelText="Custom URL"
+                            style={customUrlStyle} />;
 
         const hidden = this.state.checked ? serverURL : '';
-
-        const radioButtonStyles = {
-          block: {
-            maxWidth: 250,
-          },
-          radioButton: {
-            marginBottom: 16,
-          },
-        };
 
         const styles = {
             'width': '100%',
@@ -366,31 +380,30 @@ export default class SignUp extends Component {
                         </div>
                         <div>
                             <div>
-                            <RadioButtonGroup style={{display: 'flex',
-                              marginTop: '10px',
-                              maxWidth:'200px',
-                              flexWrap: 'wrap',
-                              margin: 'auto'}}
-                             name="server" onChange={this.handleChange}
-                             defaultSelected="standardServer">
-                            <RadioButton
-                                   value="customServer"
-                                   label="Custom Server"
-                                   labelPosition="left"
-                                   style={radioButtonStyles.radioButton}
-                                 />
-                            <RadioButton
-                                   value="standardServer"
-                                   label="Standard Server"
-                                   labelPosition="left"
-                                   style={radioButtonStyles.radioButton}
-                                 />
-                            </RadioButtonGroup>
+                            <Toggle
+                            labelPosition="right"
+                            id={'uniqueId'}
+                            labelStyle={{ zIndex: 3 }}
+                            label={this.state.checked?(
+                                        <label htmlFor={'uniqueId'}>
+                                                <div>
+                                                    {hidden}
+                                                </div>
+                                        </label>
+                            ):'Use Custom Server'}
+                            defaultToggled={false}
+                            onToggle={this.handleServeChange}
+                            style={{display: 'flex',
+                                marginTop: '10px',
+                                maxWidth:'245px',
+                                flexWrap: 'wrap',
+                                height:'28px',
+                                margin: '30px auto 0px auto'}}
+                                value="customServer"
+                            />
                             </div>
                         </div>
-                        <div>
-                        {hidden}
-                        </div>
+
                         <div>
                             <RaisedButton
                                 label="Sign Up"
@@ -399,7 +412,8 @@ export default class SignUp extends Component {
                                 backgroundColor={
                                     UserPreferencesStore.getTheme()==='light'
                                     ? '#607D8B' : '#19314B'}
-                                labelColor="#fff" />
+                                labelColor="#fff"
+                                style={{margin:'25px 0 0 0 '}} />
                         </div>
                         <h1>OR</h1>
                         <div>
