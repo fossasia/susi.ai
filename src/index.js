28fe73e4f37ce88940bce991b478799f9c59c2e4
Rishi Raj <rishiraj824@gmail.com>
2017-07-21 01:55:48 +0530

    Fixes #516 - Create a support page (#542)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Logout from './components/Auth/Logout.react';
 import ChatApp from './components/ChatApp/ChatApp.react';
 import NotFound from './components/NotFound/NotFound.react';
 import Overview from './components/Overview/Overview.react';
+import Support from './components/Support/Support.react';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -26,6 +27,7 @@ const App = () => (
 				<Route exact path="/" component={ChatApp} />
 				<Route exact path="/logout" component={Logout} />
 				<Route exact path="/overview" component={Overview} />
+				<Route exact path="/support" component={Support} />
 				<Route exact path="*" component={NotFound} />
 
 			</Switch>
