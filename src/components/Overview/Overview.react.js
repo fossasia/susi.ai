2f0d4709e83666dcc23b55394ea7ffe9e3031bfc
Rishi Raj <rishiraj824@gmail.com>
2017-07-22 03:53:31 +0530

    Fixes #563 - Change Github logo on Overview Page (#565)

--- src/components/Overview/Overview.react.js
+++ src/components/Overview/Overview.react.js
@@ -331,7 +331,7 @@ class Overview extends Component {
                       </span>
                       <span className="github_logo">
                         <Link to="https://github.com/fossasia?utf8=✓&q=susi" target="_blank">
-                        <img src='github-logo.png' alt='ghlogo' />
+                        <img src='github.png' alt='ghlogo' />
                         </Link>
                       </span>
                     </div>
