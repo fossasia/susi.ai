cb5d6b65f8a368bf3c6520182d99194d7280ab80
Isuru Abeywardana <vpisabeywardana@gmail.com>
2017-07-21 15:38:09 +0530

    Fixes: #517 -Added a terms page (#547)
    finalize terms page
    
    made for smaller screens
    
    color change
    
    resolve travis error
    
    resolve travis

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ChatApp from './components/ChatApp/ChatApp.react';
 import NotFound from './components/NotFound/NotFound.react';
 import Overview from './components/Overview/Overview.react';
 import Support from './components/Support/Support.react';
+import Terms from './components/Terms/Terms.react';
+
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -34,6 +36,7 @@ const App = () => (
 				<Route exact path="/logout" component={Logout} />
 				<Route exact path="/overview" component={Overview} />
 				<Route exact path="/support" component={Support} />
+				<Route exact path="/terms" component={Terms} />
 				<Route exact path="*" component={NotFound} />
 
 			</Switch>
