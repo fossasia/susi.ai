69727b8a6d7ea22307430411edc7bfb6cc76cd5d
Rishi Raj <rishiraj824@gmail.com>
2017-07-24 13:52:51 +0530

    Fixes #513 - Propose and implement a team page with images of participating developers

---
+++ src/components/Team/TeamList.js
@@ -0,0 +1,89 @@
+var team = [{
+	'mentors': [{
+		'name': 'Mario Behling',
+		'github': 'http://github.com/mariobehling',
+		'avatar' : 'http://fossasia.org/img/mariobehling.jpg'
+	},{
+		'name': 'Michael Christen',
+		'github': 'https://github.com/orbiter',
+		'avatar':'http://fossasia.org/img/MichaelChristen.jpg'
+	}]
+},
+{
+  'server':[{
+	'name':'Sourabh Jain',
+	'github' : 'https://github.com/saurabhjn76',
+	'avatar':'https://scontent.fmaa2-1.fna.fbcdn.net/v/t1.0-1/c80.0.480.480/284797_228629740506894_1570458_n.jpg?oh=9790e3be67ff793ab29a474b915b3d15&oe=5A0AED30'
+},{
+	'name':'Chetan Kaushik',
+	'github' : 'https://github.com/dynamitechetan',
+	'avatar':'https://avatars1.githubusercontent.com/u/14369357?v=4&s=400'
+},{
+	'name':'Dravit Lochan Gupta',
+	'github' : 'https://github.com/DravitLochan',
+	'avatar':'https://avatars0.githubusercontent.com/u/14837478?v=4&s=460'
+},{
+	'name':'Jyothir Aditya',
+	'github' : 'https://github.com/jyoth1raditya',
+	'avatar':'https://avatars2.githubusercontent.com/u/24853337?v=4&s=460'
+}]
+},
+{
+ 'android':[{
+   'name':'Chirag Wadhera',
+	'github' : 'https://github.com/chiragw15',
+	'avatar':'https://avatars1.githubusercontent.com/u/16978820?v=4&s=460'
+ },{
+   'name':'Mayank Tripathi',
+	'github' : 'https://github.com/mayank408',
+	'avatar':'https://avatars0.githubusercontent.com/u/22056321?v=4&s=460'
+ },{
+   'name':'Amit Tiwary',
+	'github' : 'https://github.com/amitiwary999',
+	'avatar':'https://avatars3.githubusercontent.com/u/14003244?v=4&s=460'
+ }]
+},
+{
+  'web' :[{
+    'name':'Rishi Raj',
+    'github' : 'http://github.com/rishiraj824',
+    'avatar':'https://avatars3.githubusercontent.com/u/11540785?v=4&s=460'
+  },{
+    'name':'Uday Theja',
+    'github' : 'https://github.com/uday96',
+    'avatar':'https://avatars2.githubusercontent.com/u/13276887?v=4&s=460'
+  },{
+    'name':'Isuru Abeywardana',
+    'github' : 'https://github.com/isuruAb',
+    'avatar':'https://avatars3.githubusercontent.com/u/7692626?v=4&s=460'
+  },{
+    'name':'Madhav Rathi',
+    'github' : 'https://github.com/madhavrathi',
+    'avatar':'https://avatars1.githubusercontent.com/u/22375731?v=4&s=460'
+  }]
+},
+{
+  'ios' : [{
+    'name' : 'Chashmeet Singh',
+    'github' : 'http://github.com/chashmeetsingh',
+    'avatar':'https://avatars1.githubusercontent.com/u/8962264?v=4&s=460'
+  }]
+},{
+  'hardware' : [{
+    'name' :'Pranjal Paliwal',
+    'github' : 'https://github.com/betterclever',
+    'avatar': 'https://avatars1.githubusercontent.com/u/16309034?v=4&s=460'
+  }]
+},{
+  'bots' :[{
+    'name' : 'Manveer Singh',
+    'github': 'https://github.com/Manveer695',
+    'avatar' : 'https://image.ibb.co/nPGCAQ/manveer.jpg'
+  },{
+    'name' : 'Ali Ayub Khan',
+    'github': 'https://github.com/AliAyub007',
+    'avatar' : 'https://avatars3.githubusercontent.com/u/25840461?v=4&s=400'
+  }]
+}]
+
+export default team;
