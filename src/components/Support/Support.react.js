54b522283df17fdfd2074f7c0ec1acf83484e9b9
Rishi Raj <rishiraj824@gmail.com>
2017-07-26 20:27:53 +0530

    Fixes #590 - Improve the Support page

--- src/components/Support/Support.react.js
+++ src/components/Support/Support.react.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './Support.css';
 import support from '../../../public/images/support.png';
-import gitter from '../../../public/images/gitter.jpg';
+import stackoverflow from '../../../public/images/stackoverflow.png';
 import googleGroups from '../../../public/images/google-groups.png';
-import github from '../../../public/images/github-logo.png';
+import github from '../../../public/images/github.png';
 import question from '../../../public/images/question.png';
 import code from '../../../public/images/code.png';
 import susi from '../../../public/images/susi.svg';
@@ -87,102 +87,104 @@ class Support extends Component {
                 </div>
                 <div className="devsite-landing-row-group">
                 <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
-                  ">
-                  <a href="https://gitter.im/fossasia/susi_server">
-                  <div className="devsite-landing-row-item-icon-container">
-                  <img alt='gitter' src={gitter} className="devsite-landing-row-item-icon"/></div></a>
-                  <div className="devsite-landing-row-item-description
-                                devsite-landing-row-item-icon-description">
-                        <a href="https://gitter.im/fossasia/susi_server">
-                        <h3 id="stack-overflow">Gitter</h3>
-                        </a>
-                        <div className="devsite-landing-row-item-description-content">
-                        Interact with us on Gitter to know more about how to
-                        start contributing.
-                        </div>
-                        </div>
-                </div>
+                    ">
+                    <a href="https://stackoverflow.com/questions/tagged/susi.ai">
+                    <div className="devsite-landing-row-item-icon-container">
+                    <img alt='gitter' src={stackoverflow} className="devsite-landing-row-item-icon"/></div></a>
+                    <div className="devsite-landing-row-item-description
+                                  devsite-landing-row-item-icon-description">
+                          <a href="https://stackoverflow.com/questions/tagged/susi.ai">
+                          <h3 id="stack-overflow">Stack Overflow</h3>
+                          </a>
+                          <div className="devsite-landing-row-item-description-content">
+                          Your resource for all technical questions and answers
+                          </div>
+                          </div>
+                  </div>
                 <div className="devsite-landing-row-item
-                devsite-landing-row-item-with-icon devsite-landing-row-item-no-image">
-                  <a href="http://groups.google.com/forum/#!forum/susiai">
-                  <div className="devsite-landing-row-item-icon-container">
-                  <img alt='google groups' src={googleGroups} className="devsite-landing-row-item-icon"/></div></a>
-                  <div className="devsite-landing-row-item-description
-                                devsite-landing-row-item-icon-description">
-                        <a href="http://groups.google.com/forum/#!forum/susiai">
-                        <h3 id="stack-overflow">Google Groups</h3>
-                        </a>
-                        <div className="devsite-landing-row-item-description-content">
-                       Discuss about the projects on our Google Groups Channel.
-                        </div>
-                        </div>
-                </div>
+                  devsite-landing-row-item-with-icon devsite-landing-row-item-no-image">
+                    <a href="http://groups.google.com/forum/#!forum/susiai">
+                    <div className="devsite-landing-row-item-icon-container">
+                    <img alt='google groups' src={googleGroups} className="devsite-landing-row-item-icon"/></div></a>
+                    <div className="devsite-landing-row-item-description
+                                  devsite-landing-row-item-icon-description">
+                          <a href="http://groups.google.com/forum/#!forum/susiai">
+                          <h3 id="stack-overflow">SUSI.AI Forums</h3>
+                          </a>
+                          <div className="devsite-landing-row-item-description-content">
+                         Discuss about the projects on our Google Groups Channel.
+                          </div>
+                          </div>
+                  </div>
                 <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
-                  ">
-                  <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi&type=&language=">
-                  <div className="devsite-landing-row-item-icon-container">
-                  <img alt='github' src={github} className="devsite-landing-row-item-icon"/></div></a>
-                  <div className="devsite-landing-row-item-description
-                                devsite-landing-row-item-icon-description">
-                        <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi&type=&language=">
-                        <h3 id="stack-overflow">Github</h3>
-                        </a>
-                        <div className="devsite-landing-row-item-description-content">
-                        Visit our repositories and start contributing.
-                        </div>
-                        </div>
-                </div>
+                    ">
+                    <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi&type=&language=">
+                    <div className="devsite-landing-row-item-icon-container">
+                    <img alt='github' src={github} className="devsite-landing-row-item-icon"/></div></a>
+                    <div className="devsite-landing-row-item-description
+                                  devsite-landing-row-item-icon-description">
+                          <a href="https://github.com/fossasia?utf8=%E2%9C%93&q=susi&type=&language=">
+                          <h3 id="stack-overflow">Github</h3>
+                          </a>
+                          <div className="devsite-landing-row-item-description-content">
+                          Visit our repositories and start contributing.
+                          </div>
+                          </div>
+                  </div>
               </div>
               </div>
               <div className="section">
               <div className='left'>
-                <div className="conversation__description">
-                  <div className="support__heading">Contact Us</div>
+                  <div className="conversation__description_">
+                    <div className="support__heading">Chat With Us</div>
+                  </div>
+                  <div className="devsite-landing-row-group">
+                  <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
+                    ">
+                    <a href="https://gitter.im/fossasia/susi_server">
+                    <div className="devsite-landing-row-item-icon-container">
+                    <img alt='question' src={question} className="devsite-landing-row-item-icon"/></div></a>
+                    <div className="devsite-landing-row-item-description
+                                  devsite-landing-row-item-icon-description">
+                          <a href="https://gitter.im/fossasia/susi_server">
+                          <h3 id="stack-overflow">Get Support</h3>
+                          </a>
+                          <div className="devsite-landing-row-item-description-content">
+                          Got a problem? We are here to help.
+                          </div>
+                          </div>
+                  </div>
                 </div>
-                <div className="devsite-landing-row-group">
-                <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
-                  ">
-                  <a href="https://github.com/fossasia/susi_server/issues/new">
-                  <div className="devsite-landing-row-item-icon-container">
-                  <img alt='question' src={question} className="devsite-landing-row-item-icon"/></div></a>
-                  <div className="devsite-landing-row-item-description
-                                devsite-landing-row-item-icon-description">
-                        <a href="https://github.com/fossasia/susi_server/issues/new">
-                        <h3 id="stack-overflow">Get Support</h3>
-                        </a>
-                        <div className="devsite-landing-row-item-description-content">
-                        Got a problem? We are here to help.
-                        </div>
-                        </div>
+                </div>
+                <div className='right'>
+                <div className="conversation__description_">
+                    <div className="support__heading">Browse, Create and Edit your own Skills</div>
+                  </div>
+                  <div className="devsite-landing-row-group">
+                  <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
+                    ">
+                    <a href="http://skills.susi.ai/">
+                    <div className="devsite-landing-row-item-icon-container">
+                    <img alt='code' src={code} className="devsite-landing-row-item-icon"/></div></a>
+                    <div className="devsite-landing-row-item-description
+                                  devsite-landing-row-item-icon-description">
+                          <a href="http://skills.susi.ai/">
+                          <h3 id="stack-overflow">Create a SUSI Dream</h3>
+                          </a>
+                          <div className="devsite-landing-row-item-description-content">
+                          You can easily create a SUSI.AI dream in the SUSI Etherpad at
+                          <a href='http://dream.susi.ai'>&nbsp;http://dream.susi.ai</a>
+                          </div>
+                          </div>
+                  </div>
+                </div>
                 </div>
               </div>
-              </div>
-              <div className='right'>
-              <div className="conversation__description">
-                  <div className="support__heading">Recent Changes & Updates</div>
-                </div>
-                <div className="devsite-landing-row-group">
-                <div className="devsite-landing-row-item devsite-landing-row-item-with-icon devsite-landing-row-item-no-image
-                  ">
-                  <a href="https://github.com/fossasia/susi_server/">
-                  <div className="devsite-landing-row-item-icon-container">
-                  <img alt='code' src={code} className="devsite-landing-row-item-icon"/></div></a>
-                  <div className="devsite-landing-row-item-description
-                                devsite-landing-row-item-icon-description">
-                        <a href="https://github.com/fossasia/susi_server/">
-                        <h3 id="stack-overflow">Change Log</h3>
-                        </a>
-                        <div className="devsite-landing-row-item-description-content">
-                        View all recent changes from the SUSI.AI Team.
-                        </div>
-                        </div>
-                </div>
-              </div>
-              </div>
-              </div>
-              <div className="section non-flex blue-background">
+              <div className="section non-flex blue-background" style={{
+                  paddingBottom: '200px'
+                }}>
                 <div className="conversation__description footer-desc">
-                  <div className="support__heading center">Get Started Today</div>
+                  <div className="support__heading center blue-text">Get Started Today</div>
 
                   <RaisedButton label="Sign Up" onTouchTap={this.handleSignUp} style={style} />
                 </div>
