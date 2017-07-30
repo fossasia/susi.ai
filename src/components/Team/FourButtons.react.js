d383b68a081055c96347249298eb57302585aa44
Isuru Abeywardana <vpisabeywardana@gmail.com>
2017-07-30 20:16:10 +0530

    Fixes: #603 - Improve team page and add more data (#617)
    * Fixes: #603 - Improve team page and add more data
    
    sort members and add animation
    
    sort members and add animation
    
    sort members
    
    data added
    
    add data
    
    add data
    
    removed comments
    
    add responsive buttons
    
    add footer logo
    
    add saurabhs name
    
    add images and hide buttons
    
    add daminis avatar
    
    add jigyasa's avatar
    
    add data
    
    add data
    
    * add manveer

---
+++ src/components/Team/FourButtons.react.js
@@ -0,0 +1,66 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import IconButton from 'material-ui/IconButton';
+import { CardActions } from 'material-ui/Card';
+
+class FourButtons extends Component {
+
+    render() {
+
+        const flatButtonStyle = {
+            minWidth: '25%',
+            fontSize: '25px',
+            color: '#fff',
+            padding: '0%',
+           marginRight:'5px',
+           marginLeft:'5px'
+        };
+        const flatButtonStyleNone = {
+            minWidth: '10%',
+            fontSize: '25px',
+            color: '#fff',
+            padding: '0%',
+            display: 'none'
+        };
+        let member= this.props.member;
+        return (
+            <div>
+          <CardActions>
+            <IconButton
+              style={member.github==='#'?flatButtonStyleNone:flatButtonStyle}
+              href={member.github}
+              target="_blank"
+            >
+              <i className="fa fa-github"></i>
+            </IconButton>
+            <IconButton
+              style={member.twitter==='#'?flatButtonStyleNone:flatButtonStyle}
+              href={member.twitter}
+              target="_blank"
+            >
+              <i className="fa fa-twitter"></i>
+            </IconButton>
+            <IconButton
+              style={member.linkedin==='#'?flatButtonStyleNone:flatButtonStyle}
+              href={member.linkedin}
+              target="_blank"
+            >
+              <i className="fa fa-linkedin-square"></i>
+            </IconButton>
+            <IconButton
+              style={member.blog==='#'?flatButtonStyleNone:flatButtonStyle}
+              href={member.blog}
+              target="_blank"
+            >
+              <i className="fa fa-globe"></i>
+            </IconButton>
+          </CardActions>
+            </div>
+        )
+    }
+}
+FourButtons.propTypes = {
+    member: PropTypes.object
+}
+export default FourButtons;
