a38260759b8d4ea97ef769c9617c23e2c88f6640
Madhav Rathi <mdhvrthi@gmail.com>
2017-08-04 17:20:06 +0530

    Fix styling of cards and added publish date

--- src/components/Blog/Blog.react.js
+++ src/components/Blog/Blog.react.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import $ from 'jquery';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
+import susi from '../../images/susi-logo.svg';
+import dateFormat from 'dateformat';
 import StaticAppBar from '../StaticAppBar/StaticAppBar.react';
 
 class Blog extends Component {
@@ -40,36 +41,82 @@ class Blog extends Component {
 
 
     render() {
-        let post;
         if(this.state.postRendered) {
             console.log('Hi');
-            post = this.state.posts.map((posts , i) => {
-                let text = posts.description.split('…');
-                    return (
-                        <div key={i} className="section">
-                            <Card  className='blog-card'>
-                                <CardMedia >
-                                </CardMedia>
-                                <CardTitle title={posts.title} subtitle={'by '+ posts.author} />
-                                <CardText>{text[0]+'...'} </CardText>
-                                <CardActions>
-                                    <FlatButton href={posts.link} label="Read More" />
-                                </CardActions>
-                            </Card>
-                        </div>
-                    )
-            })
         }
         console.log(this.state.posts);
-        return (
-            <div>
-                <StaticAppBar {...this.props}
-                    location={this.props.location} />
-                    <div className="top-card">
-                        {post}
-                    </div>
-            </div>
-        );
+        if(this.state.postRendered) {
+            return (
+                <div>
+                    <StaticAppBar {...this.props}
+                        location={this.props.location} />
+                        <div className='head_section'>
+                            <div className='container'>
+                                <div className="heading">
+                                    <h1>Blogs</h1>
+                                    <p>Latest Blogs on SUSI.AI</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="top-card">
+                            {
+                                this.state.posts.map((posts , i) => {
+                                    let text = posts.description.split('…');
+                                    let date = posts.pubDate.split(' ');
+                                    let d = new Date(date[0]);
+                                        return (
+                                            <div key={i} className="section_copy">
+                                                <Card  className='blog-card'>
+                                                    <CardMedia
+                                                        overlay={
+                                                            <CardTitle
+                                                                subtitle={'Published on '+ dateFormat(d, 'dddd, mmmm dS, yyyy')} />
+                                                        }
+                                                    >
+                                                        <img
+                                                            src={susi}
+                                                             alt={posts.title}
+                                                        />
+                                                    </CardMedia>
+                                                    <CardTitle title={posts.title} subtitle={'by '+ posts.author} />
+                                                    <CardText>{text[0]+'...'} </CardText>
+                                                    <CardActions>
+                                                        <FlatButton href={posts.link} label="Read More" />
+                                                    </CardActions>
+                                                </Card>
+                                            </div>
+                                        )
+                                })
+                            }
+                        </div>
+                        <div className='footer'>
+                            <a className='susi-logo-anchor' href='/overview'>
+                                <img src={susi} alt='SUSI' className='susi-logo' />
+                            </a>
+                            <div className="footer_content">
+                                <div className='footer-container'>
+                                    <ul className='alignLeft'>
+                                        <li><a href='/overview'>Overview</a></li>
+                                        <li><a href='/blog'>Blog</a></li>
+                                        <li><a href='https://github.com/fossasia?utf8=%E2%9C%93&q=susi'>Code</a></li>
+                                    </ul>
+                                    <ul className='alignRight'>
+                                        <li><a href='/settings'>Settings</a></li>
+                                        <li><a href='/terms'>Terms</a></li>
+                                        <li><a href='/contact'>Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            );
+        }
+            return (
+                <div>
+                    <StaticAppBar {...this.props}
+                        location={this.props.location} />
+                </div>
+            );
     }
 }
 Blog.propTypes = {
