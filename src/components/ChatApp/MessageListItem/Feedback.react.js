d5b0d9dde57313c6d7f24eddaae423dbbc771af1
chick4nnn <chickenz.th@gmail.com>
2017-12-12 14:21:29 +0700

    Update feedback!

--- src/components/ChatApp/MessageListItem/Feedback.react.js
+++ src/components/ChatApp/MessageListItem/Feedback.react.js
@@ -47,7 +47,7 @@ class Feedback extends React.Component {
 			case 'positive':{
 				this.setState({
 					ratingGiven: true,
-					positive: (this.state.positive) ? false : true,
+					positive: !this.state.positive,
 					negative: false,
 				});
 				break;
@@ -56,7 +56,7 @@ class Feedback extends React.Component {
 				this.setState({
 					ratingGiven: true,
 					positive: false,
-					negative: (this.state.negative) ? false : true,
+					negative: !this.state.negative,
 				});
 				break;
 			}
