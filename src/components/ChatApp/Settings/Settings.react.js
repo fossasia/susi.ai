bc3928cb7ebe7e341a292e8f38ca40b7b5115ad5
Naveen Rajan <naveentherocker1234@gmail.com>
2018-01-28 12:33:58 +0530

    Fixed #975: Fixed Sidebar Overflowing (#976)
    * Fixed Sidebar Overflowing
    
    * Small Bug
    
    * Bug Again
    
    * Fixed Travis

--- src/components/ChatApp/Settings/Settings.react.js
+++ src/components/ChatApp/Settings/Settings.react.js
@@ -1179,7 +1179,8 @@ class Settings extends Component {
 				<hr className="break-line"/>
 				<MenuItem style={{color:themeForegroundColor}} value='Connect to SUSI Hardware' className="setting-item"  leftIcon={<HardwareIcon color={menuIconColor}/>}>Connect to SUSI Hardware<ChevronRight style={{color:themeForegroundColor}} className="right-chevron"/></MenuItem>
 				<hr className="break-line"/>
-				<MenuItem style={{color:themeForegroundColor}} value='Share on Social media' className="setting-item" leftIcon={<ShareIcon color={menuIconColor}/>}>Share on Social media<ChevronRight style={{color:themeForegroundColor}} className="right-chevron"/></MenuItem>
+				<MenuItem style={{color:themeForegroundColor}} value='Share on Facebook' className="setting-item" leftIcon={<ShareIcon color={menuIconColor}/>}>Share on Facebook<ChevronRight style={{color:themeForegroundColor}} className="right-chevron"/></MenuItem>
+				<hr className="break-line"/>
 		</Menu>
 		</div>
 		<div className="settings-list-dropdown">
