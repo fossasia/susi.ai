d4311cb126e098afc9a495e4604545e9d5018505
Rohan Singh <rohaniiitv@gmail.com>
2018-04-13 02:43:51 +0530

    Modified Dark Background in Chat Section

--- src/components/ChatApp/Settings/Settings.react.js
+++ src/components/ChatApp/Settings/Settings.react.js
@@ -278,6 +278,15 @@ class Settings extends Component {
 		if (this.preview) {
 			vals.theme = UserPreferencesStore.getTheme(!this.preview);
 			vals.previewTheme = this.state.theme;
+			var SettingsContainer = document.getElementById("settings-container");
+			if(vals.previewTheme==='dark')
+				{
+					SettingsContainer.style.background = "#000012";
+				}
+			else
+			{
+					SettingsContainer.style.background = "#f2f2f2";
+			}
 		}
 		// else save current theme state to theme
 		else {
@@ -562,6 +571,14 @@ class Settings extends Component {
 			search: false,
 		});
 
+		if(this.state.theme==='dark')
+			{
+				document.body.style.background = '#000012';
+			}
+		else
+			{
+				document.body.style.background = '#f2f2f2';
+			}
 		this.showWhenLoggedIn = 'none';
 		let searchParams = new URLSearchParams(window.location.search);
 		let tab = searchParams.get('tab');
@@ -1334,7 +1351,7 @@ class Settings extends Component {
 	// to check if something has been modified or not
 	let somethingToSave=this.getSomethingToSave();
 		return (
-			<div className="settings-container">
+			<div className="settings-container" id="settings-container">
 		<StaticAppBar settings={this.state.intialSettings} {...this.props}
 			location={this.props.location} />
 				<div className='settingMenu'>
