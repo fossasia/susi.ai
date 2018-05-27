b434dd1611e60a2de2bfdfe754a824a3e174bcce
Akshat Jain <akjn11@gmail.com>
2018-05-28 02:58:55 +0530

    Added Devices tab in Settings to display list of connected devices (#1250)

--- src/__tests__/components/HardwareComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import HardwareComponent from '../../components/ChatApp/HardwareComponent';
-import { shallow } from 'enzyme';
-
- it('render HardwareComponent without crashing',()=>{
-   shallow(<HardwareComponent />);
- });
