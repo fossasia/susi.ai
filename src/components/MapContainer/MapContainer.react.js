fa911283842f4aceeefeab25aa1e0bb9fa6c6995
Akshat-Jain <akjn11@gmail.com>
2018-06-29 02:11:10 +0530

    Display device information on clicking the marker in the map

--- src/components/MapContainer/MapContainer.react.js
+++ src/components/MapContainer/MapContainer.react.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line
+import { InfoWindow } from 'google-maps-react';
+
 import PropTypes from 'prop-types';
 
 export default class MapContainer extends Component {
@@ -27,6 +30,8 @@ export default class MapContainer extends Component {
       );
 
       this.map = new maps.Map(node, mapConfig);
+      let infoWindow = new google.maps.InfoWindow();
+      let i = 0;
 
       // Add markers to map
       this.props.mapData.forEach(location => {
@@ -34,8 +39,27 @@ export default class MapContainer extends Component {
         const marker = new google.maps.Marker({
           position: { lat: location.location.lat, lng: location.location.lng },
           map: this.map,
-          title: location.name,
+          title: 'Click to see device information.',
+          devicename: this.props.devicenames[i],
+          room: this.props.rooms[i],
+          macid: this.props.macids[i],
         });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.setContent(
+            'Mac Address: ' +
+              marker.macid +
+              '<br/>' +
+              'Room: ' +
+              marker.room +
+              '<br/>' +
+              'Device name: ' +
+              marker.devicename,
+          );
+          infoWindow.open(this.map, marker);
+        });
+
+        i++;
       });
     }
   }
@@ -59,5 +83,8 @@ MapContainer.propTypes = {
   centerLat: PropTypes.number,
   centerLng: PropTypes.number,
   mapData: PropTypes.array,
+  devicenames: PropTypes.array,
+  rooms: PropTypes.array,
+  macids: PropTypes.array,
   google: PropTypes.object,
 };
