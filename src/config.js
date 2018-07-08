60cb9a852a02386acf1222858fd48910c9a07867
Shubham Gupta <shubhamg@outlook.com>
2018-07-08 14:52:05 +0530

    Add Blog RSS-JSON API Key in Config.js #1425 (#1426)

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const KEY = '6LfPZGAUAAAAANY4ZLR_lSat_YCuqqcV3sl84ulW';
+export const CAPTCHA_KEY = '6LfPZGAUAAAAANY4ZLR_lSat_YCuqqcV3sl84ulW';
 export const MAP_KEY = 'AIzaSyCWxXuqny-dx-1FiMrjCSr6fFvukoy7oEM';
+export const BLOG_KEY = 'qsmzjtyycc49whsfvf5ikzottxrbditq3burojhd';
