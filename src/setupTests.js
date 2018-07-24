a99438dcd66f9e1f8f54cfc3c65ca18183e68283
Shubham Gupta <shubhamg@outlook.com>
2018-07-25 00:23:34 +0530

    Add adapter in config

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,8 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 window.matchMedia =
   window.matchMedia ||
   function() {
