df97aa7a8756f50191f041254c2becd82213bcc2
Rohan Singh <rohaniiitv@gmail.com>
2018-09-27 20:51:17 +0530

    Fixed Travis CI build

--- src/components/ChatApp/Settings/Settings.react.js
+++ src/components/ChatApp/Settings/Settings.react.js
@@ -772,8 +772,8 @@ class Settings extends Component {
   // Handle change to theme settings
   handleSelectChange = (event, value) => {
     value === 'light' || value === 'custom'
-      ? ($('#settings-container').addClass({"class":"settings-container-light"}))
-      : ($('#settings-container').addClass({"class":"settings-container-dark"}));
+      ? ($('#settings-container').addClass({ 'class': 'settings-container-light' }))
+      : ($('#settings-container').addClass({ 'class': 'settings-container-dark' }));
     this.preview = true;
     this.setState({ theme: value }, () => {
       this.handleSubmit();
@@ -2223,7 +2223,7 @@ class Settings extends Component {
       <div
         id="settings-container"
         className={
-          (UserPreferencesStore.getTheme() === 'light' && this.state.settingNo!=='Theme') || (this.state.settingNo==='Theme' && this.state.theme==='light')
+          (UserPreferencesStore.getTheme() === 'light' && this.state.settingNo !== 'Theme') || (this.state.settingNo === 'Theme' && this.state.theme === 'light')
             ? 'settings-container-light'
             : 'settings-container-dark'
         }
