d49076c8843efb5209bf7be73e4090acb4296c1b
samagragupta <samagraguptajpr@gmail.com>
2018-09-28 17:49:30 +0530

    Forgot password link fixed

--- src/components/Auth/ChangePassword/ChangePassword.react.js
+++ src/components/Auth/ChangePassword/ChangePassword.react.js
@@ -11,7 +11,7 @@ import zxcvbn from 'zxcvbn';
 import Cookies from 'universal-cookie';
 import Close from 'material-ui/svg-icons/navigation/close';
 import Translate from '../../Translate/Translate.react';
-import urls from '../../../utils/urls';
+import ForgotPassword from '../ForgotPassword/ForgotPassword.react';
 
 const cookies = new Cookies();
 
@@ -32,6 +32,7 @@ export default class ChangePassword extends Component {
       success: false,
       validForm: false,
       msgOpen: false,
+      showForgotPwd: false,
     };
 
     this.emailErrorMessage = '';
@@ -61,6 +62,20 @@ export default class ChangePassword extends Component {
         msgOpen: false,
       });
     }
+  };
+
+  handleClose = event => {
+    this.setState({
+      showDialog: false,
+      showForgotPwd: false,
+    });
+  };
+
+  handleForgotPwd = event => {
+    event.preventDefault();
+    this.setState({
+      showForgotPwd: true,
+    });
   };
 
   // Handle changes to current, new and confirm new passwords
@@ -307,9 +322,7 @@ export default class ChangePassword extends Component {
                 visibilityIconStyle={{ display: 'none' }}
               />
               <div className="forgot">
-                <a href={`${urls.ACCOUNT_URL}/forgotpwd`}>
-                  Forgot your password?
-                </a>
+                <a onClick={this.handleForgotPwd}>Forgot your password?</a>
               </div>
             </div>
             <br />
@@ -358,6 +371,19 @@ export default class ChangePassword extends Component {
             </div>
           </form>
         </Paper>
+
+        {/* Forgot Password Modal */}
+        <div className="ModalDiv">
+          <Dialog
+            modal={false}
+            open={this.state.showForgotPwd}
+            onRequestClose={this.handleClose}
+            className="ModalDiv"
+          >
+            <ForgotPassword closeModal={this.handleClose.bind(this)} />
+          </Dialog>
+        </div>
+
         {this.state.msg && (
           <div>
             <Dialog
