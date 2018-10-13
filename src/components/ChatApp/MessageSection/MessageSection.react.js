cbe82c69a56a64f9cafc149a62423360d50a9b02
samagragupta <samagraguptajpr@gmail.com>
2018-10-13 15:20:10 +0530

    fixed first click on down arrow key

--- src/components/ChatApp/MessageSection/MessageSection.react.js
+++ src/components/ChatApp/MessageSection/MessageSection.react.js
@@ -903,11 +903,34 @@ class MessageSection extends Component {
         searchState: currState,
       });
     }
-    if (markedIDs && ul && newIndex <= 0) {
+    if (markedIDs && ul && newIndex === 0) {
       let currState = this.state.searchState;
       newIndex = indexLimit;
       currState.scrollIndex = newIndex;
       currState.searchIndex = 1;
+    }
+    if (markedIDs && ul && newIndex < 0) {
+      let currState = this.state.searchState;
+      newIndex = indexLimit;
+      currState.scrollIndex = newIndex;
+      currState.searchIndex = 1;
+      newIndex = this.state.searchState.scrollIndex - 1;
+      newSearchCount = this.state.searchState.searchIndex - 1;
+      markedIDs = this.state.searchState.markedIDs;
+      indexLimit = this.state.searchState.scrollLimit;
+      ul = this.messageList;
+      if (newSearchCount <= 0) {
+        newSearchCount = indexLimit;
+      }
+      if (markedIDs && ul && newIndex >= 0) {
+        currState = this.state.searchState;
+        currState.scrollIndex = newIndex;
+        currState.searchIndex = newSearchCount;
+        currState.scrollID = markedIDs[newIndex];
+        this.setState({
+          searchState: currState,
+        });
+      }
     }
   };
 
