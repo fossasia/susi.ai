218d5a72c751c792fc05f467774c6437a234eec7
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-11-05 16:28:36 +0530

    Fixed #1687: refactored Login.react.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Blog from './components/Blog/Blog.react';
 import ChatApp from './components/ChatApp/ChatApp.react';
 import Contact from './components/Contact/Contact.react';
 import Devices from './components/Devices/Devices.react';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Logout from './components/Auth/Logout.react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import PropTypes from 'prop-types';
 import NotFound from './components/NotFound/NotFound.react';
 import Overview from './components/Overview/Overview.react';
 import Settings from './components/ChatApp/Settings/Settings.react';
@@ -13,8 +15,6 @@ import Support from './components/Support/Support.react';
 import Team from './components/Team/Team.react';
 import Terms from './components/Terms/Terms.react';
 import Privacy from './components/Privacy/Privacy.react';
-import { Switch, Route } from 'react-router-dom';
-import React, { Component } from 'react';
 
 const muiTheme = getMuiTheme({
   toggle: {
