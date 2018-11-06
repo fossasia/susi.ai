74e168a0aa2e251c8c47e7c5e8e0741fbd072b26
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-11-06 20:31:00 +0530

    Fixes  #1756: Integrated Redux into ChangePassword.react.js

--- src/__tests__/components/Auth/ChangePassword/ChangePassword.js
+++ src/__tests__/components/Auth/ChangePassword/ChangePassword.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ChangePassword from '../../../../components/Auth/ChangePassword/ChangePassword.react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<ChangePassword />', () => {
   it('render ChangePassword without crashing', () => {
-    shallow(<ChangePassword />);
+    shallow(
+      <Provider store={store}>
+        <ChangePassword />
+      </Provider>,
+    );
   });
 });
