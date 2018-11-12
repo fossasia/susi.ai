6a1dc0cae777943f6498739c236602984b7db586
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-11-12 19:39:14 +0530

    Fixed #1717:MessageSection.react.js refactored and connected to store

--- src/__tests__/components/ChatApp/MessageSection/MessageSection.js
+++ src/__tests__/components/ChatApp/MessageSection/MessageSection.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import MessageSection from '../../../../components/ChatApp/MessageSection/MessageSection.react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<MessageSection />', () => {
   it('render MessageSection without crashing', () => {
-    shallow(<MessageSection />);
+    shallow(
+      <Provider store={store}>
+        <MessageSection />
+      </Provider>,
+    );
   });
 });
