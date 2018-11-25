3a53e00bdc6905350847ff1d83a1f7e239fb13c5
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-11-25 14:23:40 +0530

    Fixes #1758: Login loading animation stops if login fails

--- src/index.js
+++ src/index.js
@@ -51,9 +51,15 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//matomo.anomic.de/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  var u = '//matomo.anomic.de/';
+  _paq.push(['setTrackerUrl', u + 'piwik.php']);
   _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  var d = document,
+    g = d.createElement('script'),
+    s = d.getElementsByTagName('script')[0];
+  g.type = 'text/javascript';
+  g.async = true;
+  g.defer = true;
+  g.src = u + 'piwik.js';
+  s.parentNode.insertBefore(g, s);
 })();
