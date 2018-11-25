6eda3b40dd9699231c091af628eb60cd03804080
humble_D <aakashs.mallik.cse15@itbhu.ac.in>
2018-11-25 21:50:54 +0000

    Fixes #1753: Redux integration for Forgot password component (#1755)

--- src/redux/actionTypes/index.js
+++ src/redux/actionTypes/index.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 export default _.mapKeys([
   'APP_GET_LOGIN',
   'APP_GET_API_KEYS',
+  'APP_GET_FORGOT_PASSWORD',
   'SETTINGS_GET_USER_SETTINGS',
   'SETTINGS_REMOVE_USER_DEVICE',
   'SETTINGS_ADD_USER_DEVICE',
