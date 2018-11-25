8c2d4615feffebbefeeaca5c8de60e05a8380514
Michael Christen <Orbiter@users.noreply.github.com>
2018-11-25 02:05:06 +0100

    added matomo snippet

--- src/index.js
+++ src/index.js
@@ -46,3 +46,14 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
+
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//matomo.anomic.de/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', '2']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
