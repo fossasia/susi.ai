9a15d0d86586d96ca7b86df56d175a5067e9309d
Shubham Gupta <shubhamg@outlook.com>
2018-12-09 19:00:47 +0530

    Add reducers, actionCreators, actionTypes for Settings#1802

--- src/redux/actions/settings.js
+++ src/redux/actions/settings.js
@@ -2,9 +2,9 @@ import { createAction } from 'redux-actions';
 import actionTypes from '../actionTypes';
 import * as apis from '../../apis';
 
-// const returnArgumentsFn = function(payload) {
-//   return Promise.resolve(payload);
-// };
+const returnArgumentsFn = function(payload) {
+  return Promise.resolve(payload);
+};
 
 export default {
   getUserSettings: createAction(
@@ -18,5 +18,54 @@ export default {
   addUserDevice: createAction(
     actionTypes.SETTINGS_ADD_USER_DEVICE,
     apis.addUserDevice,
+  ),
+  setUserName: createAction(
+    actionTypes.SETTINGS_SET_USER_NAME,
+    returnArgumentsFn,
+  ),
+  setEmailID: createAction(actionTypes.SETTINGS_SET_EMAILID, returnArgumentsFn),
+  setTimezone: createAction(
+    actionTypes.SETTINGS_SET_TIMEZONE,
+    returnArgumentsFn,
+  ),
+  setDefaultLanguage: createAction(
+    actionTypes.SETTINGS_SET_DEFAULT_LANGUAGE,
+    returnArgumentsFn,
+  ),
+  setPhoneNumber: createAction(
+    actionTypes.SETTINGS_SET_PHONE_NUMBER,
+    returnArgumentsFn,
+  ),
+  setCountryCode: createAction(
+    actionTypes.SETTINGS_SET_COUNTRY_CODE,
+    returnArgumentsFn,
+  ),
+  setCountryDialCode: createAction(
+    actionTypes.SETTINGS_SET_COUNTRY_DIALCODE,
+    returnArgumentsFn,
+  ),
+  toggleEnterToSendMessage: createAction(
+    actionTypes.SETTINGS_TOGGLE_ENTER_AS_SEND,
+  ),
+  setTheme: createAction(actionTypes.SETTINGS_SET_THEME, returnArgumentsFn),
+  toggleCustomThemeModal: createAction(
+    actionTypes.SETTINGS_TOGGLE_CUSTOM_THEME,
+  ),
+  toggleMicInput: createAction(actionTypes.SETTINGS_TOGGLE_MIC_INPUT),
+  toggleSpeechOutput: createAction(actionTypes.SETTINGS_TOGGLE_SPEECH_OUTPUT),
+  toggleSpeechOutputAlwaysOn: createAction(
+    actionTypes.SETTINGS_SET_OUTPUT_ALWAYS_ON,
+  ),
+  setSpeechOutputRate: createAction(
+    actionTypes.SETTINGS_SET_SPEECH_OUTPUT_RATE,
+    returnArgumentsFn,
+  ),
+  setSpeechOutputPitch: createAction(
+    actionTypes.SETTINGS_SET_SPEECH_OUTPUT_PITCH,
+    returnArgumentsFn,
+  ),
+  setSpeechOutputTTSLanguage: createAction(
+    actionTypes.SETTINGS_SET_SPEECH_OUTPUT_TTSLANGUAGE,
+    returnArgumentsFn,
   ),
 };
--- src/redux/reducers/settings.js
+++ src/redux/reducers/settings.js
@@ -4,6 +4,7 @@ import actionTypes from '../actionTypes';
 const defaultState = {
   theme: 'light',
   previewTheme: 'light',
+  themeOpen: false,
   server: '',
   standardServer: '',
   enterAsSend: true,
@@ -41,6 +42,121 @@ export default handleActions(
     [actionTypes.SETTINGS_ADD_USER_DEVICE](state, { payload }) {
       return {
         ...state,
+      };
+    },
+    // Account
+    [actionTypes.SETTINGS_SET_USER_NAME](state, { payload }) {
+      const { userName } = payload;
+      return {
+        ...state,
+        userName,
+      };
+    },
+    [actionTypes.SETTINGS_SET_EMAILID](state, { payload }) {
+      // identityName = emailId
+      const { email } = payload;
+      return {
+        ...state,
+        email,
+      };
+    },
+    [actionTypes.SETTINGS_SET_TIMEZONE](state, { payload }) {
+      const { timeZone } = payload;
+      return {
+        ...state,
+        timeZone,
+      };
+    },
+    [actionTypes.SETTINGS_SET_DEFAULT_LANGUAGE](state, { payload }) {
+      const { prefLanguage } = payload;
+      return {
+        ...state,
+        prefLanguage,
+      };
+    },
+    // Mobile
+    [actionTypes.SETTINGS_SET_PHONE_NUMBER](state, { payload }) {
+      const { phoneNo } = payload;
+      return {
+        ...state,
+        phoneNo,
+      };
+    },
+    [actionTypes.SETTINGS_SET_COUNTRY_CODE](state, { payload }) {
+      const { countryCode } = payload;
+      return {
+        ...state,
+        countryCode,
+      };
+    },
+    [actionTypes.SETTINGS_SET_COUNTRY_DIALCODE](state, { payload }) {
+      const { countryDialCode } = payload;
+      return {
+        ...state,
+        countryDialCode,
+      };
+    },
+    // ChatApp
+    [actionTypes.SETTINGS_TOGGLE_ENTER_AS_SEND](state, { payload }) {
+      return {
+        ...state,
+        enterAsSend: !state.enterAsSend,
+      };
+    },
+    // Theme
+    [actionTypes.SETTINGS_SET_THEME](state, { payload }) {
+      const { theme } = payload;
+      return {
+        ...state,
+        theme,
+      };
+    },
+    [actionTypes.SETTINGS_TOGGLE_CUSTOM_THEME](state, { payload }) {
+      // themeOpen = open custom theme changer
+      return {
+        ...state,
+        themeOpen: !state.themeOpen,
+      };
+    },
+    // Microphone
+    [actionTypes.SETTINGS_TOGGLE_MIC_INPUT](state, { payload }) {
+      return {
+        ...state,
+        micInput: !state.micInput,
+      };
+    },
+    // Speech
+    [actionTypes.SETTINGS_TOGGLE_SPEECH_OUTPUT](state, { payload }) {
+      return {
+        ...state,
+        speechOutput: !state.speechOutput,
+      };
+    },
+    [actionTypes.SETTINGS_SET_OUTPUT_ALWAYS_ON](state, { payload }) {
+      return {
+        ...state,
+        speechOutputAlways: !state.speechOutputAlways,
+      };
+    },
+    [actionTypes.SETTINGS_SET_SPEECH_OUTPUT_RATE](state, { payload }) {
+      const { speechRate } = payload;
+      return {
+        ...state,
+        speechRate,
+      };
+    },
+    [actionTypes.SETTINGS_SET_SPEECH_OUTPUT_PITCH](state, { payload }) {
+      const { speechPitch } = payload;
+      return {
+        ...state,
+        speechPitch,
+      };
+    },
+    [actionTypes.SETTINGS_SET_SPEECH_OUTPUT_TTSLANGUAGE](state, { payload }) {
+      const { ttsLanguage } = payload;
+      return {
+        ...state,
+        ttsLanguage,
       };
     },
   },
