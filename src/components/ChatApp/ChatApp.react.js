cdaadabb7f42e30c5484f5faedc520f1e9013f27
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-12-10 04:47:32 +0530

    Fixes #1811: Logout action added, force refresh has been removed

--- src/components/ChatApp/ChatApp.react.js
+++ src/components/ChatApp/ChatApp.react.js
@@ -2,7 +2,6 @@ import MessageSection from './MessageSection/MessageSection.react';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ChatApp.css';
-import history from '../../history';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import actions from '../../redux/actions/messages';
@@ -11,23 +10,29 @@ import { createMessagePairArray } from '../../utils/formatMessage';
 class ChatApp extends Component {
   static propTypes = {
     actions: PropTypes.object,
+    app: PropTypes.object,
   };
 
+  constructor(props) {
+    super(props);
+    const { uuid } = props.app;
+    this.state = {
+      uuid,
+    };
+  }
+
   componentDidMount() {
-    document.title = 'SUSI.AI Chat - Open Source Artificial Intelligence';
-    // force an update if the URL changes
-    history.listen(() => this.forceUpdate());
-    this.props.actions
-      .initializeMessageStore()
+    const { getHistoryFromServer, initializeMessageStore } = this.props.actions;
+    getHistoryFromServer()
       .then(({ payload }) => {
         createMessagePairArray(payload).then(messagePairArray => {
-          console.log(messagePairArray);
-          return this.props.actions.createHistoryMessages(messagePairArray);
+          initializeMessageStore(messagePairArray);
         });
       })
       .catch(error => {
         console.log(error);
       });
+    document.title = 'SUSI.AI Chat - Open Source Artificial Intelligence';
   }
 
   render() {
@@ -39,6 +44,12 @@ class ChatApp extends Component {
   }
 }
 
+function mapStateToProps({ app }) {
+  return {
+    app,
+  };
+}
+
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(actions, dispatch),
@@ -46,6 +57,6 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(ChatApp);
