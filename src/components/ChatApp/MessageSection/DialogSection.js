92e016634e313f5ed664e8ab1b77e72b9b8fd5d7
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-12-11 04:29:31 +0530

    Fixes #1817: Login, Signup and ForgotPassword component shifted to common parent component

--- src/components/ChatApp/MessageSection/DialogSection.js
+++ src/components/ChatApp/MessageSection/DialogSection.js
@@ -1,8 +1,5 @@
 import Dialog from 'material-ui/Dialog';
 import React from 'react';
-import Login from '../../Auth/Login/Login.react';
-import SignUp from '../../Auth/SignUp/SignUp.react';
-import ForgotPassword from '../../Auth/ForgotPassword/ForgotPassword.react';
 import PropTypes from 'prop-types';
 import Close from 'material-ui/svg-icons/navigation/close';
 
@@ -19,52 +16,6 @@ const DialogSection = props => {
   };
   return (
     <div>
-      {/* Login */}
-      <Dialog
-        className="dialogStyle"
-        modal={false}
-        open={props.openLogin}
-        autoScrollBodyContent={true}
-        bodyStyle={props.bodyStyle}
-        contentStyle={{ width: '35%', minWidth: '300px' }}
-        onRequestClose={props.onRequestClose()}
-      >
-        <Login
-          {...props}
-          handleForgotPassword={props.onForgotPassword()}
-          handleSignUp={props.handleSignUp}
-        />
-        <Close style={closingStyle} onTouchTap={props.onRequestClose()} />
-      </Dialog>
-      {/* SignUp */}
-      <Dialog
-        className="dialogStyle"
-        modal={false}
-        open={props.openSignUp}
-        autoScrollBodyContent={true}
-        bodyStyle={props.bodyStyle}
-        contentStyle={{ width: '35%', minWidth: '300px' }}
-        onRequestClose={props.onRequestClose()}
-      >
-        <SignUp
-          {...props}
-          onRequestClose={props.onRequestClose()}
-          onLoginSignUp={props.onLoginSignUp()}
-        />
-        <Close style={closingStyle} onTouchTap={props.onRequestClose()} />
-      </Dialog>
-      {/*  Forgot Password */}
-      <Dialog
-        className="dialogStyle"
-        modal={false}
-        open={props.openForgotPassword}
-        autoScrollBodyContent={true}
-        contentStyle={{ width: '35%', minWidth: '300px' }}
-        onRequestClose={props.onRequestClose()}
-      >
-        <ForgotPassword {...props} onLoginSignUp={props.onLoginSignUp()} />
-        <Close style={closingStyle} onTouchTap={props.onRequestClose()} />
-      </Dialog>
       <Dialog
         className="dialogStyle"
         contentStyle={{
@@ -89,21 +40,8 @@ const DialogSection = props => {
 };
 
 DialogSection.propTypes = {
-  openLogin: PropTypes.bool,
-  openSignUp: PropTypes.bool,
-  openForgotPassword: PropTypes.bool,
-  openHardwareChange: PropTypes.bool,
   tour: PropTypes.bool,
-  onLoginSignUp: PropTypes.func,
-  handleSignUp: PropTypes.func,
-  ServerChangeActions: PropTypes.array,
-  HardwareActions: PropTypes.array,
-  bodyStyle: PropTypes.object,
-  onRequestClose: PropTypes.func,
   onRequestCloseTour: PropTypes.func,
-  onSaveThemeSettings: PropTypes.func,
-  onForgotPassword: PropTypes.func,
-  onSignedUp: PropTypes.func,
 };
 
 export default DialogSection;
