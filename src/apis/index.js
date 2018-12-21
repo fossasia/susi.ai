96e288a8428411eac26191da514fe50c8292b07c
Shubham Gupta <shubhamg@outlook.com>
2018-12-21 17:43:22 +0530

    Remove jQuery from PreviewThemeChat, Blog, MessageComposer #1812

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,11 @@
 import ajax from '../helpers/ajax';
 import urls from '../utils/urls';
+import { BLOG_KEY } from '../config.js';
+// import { Cookies } from 'react-cookie';
+// import actions from '../redux/actions/messages';
 const { API_URL } = urls;
 const AUTH_API_PREFIX = 'aaa';
+const CHAT_API_PREFIX = 'susi';
 
 /* Export API calls from this file
 Example function -
@@ -54,4 +58,54 @@ export function getForgotPassword(payload) {
   const { email } = payload;
   const url = `${API_URL}/${AUTH_API_PREFIX}/recoverpassword.json`;
   return ajax.get(url, { forgotemail: email });
+}
+
+export function getSusiReply(payload) {
+  console.log(payload);
+  const { message } = payload;
+
+  const url = `${API_URL}/${CHAT_API_PREFIX}/chat.json`;
+  const locale = document.documentElement.getAttribute('lang');
+
+  // add location information if available
+  // TODO
+
+  // check for connectivity only then call this
+  // TODO
+
+  return ajax.get(url, {
+    q: encodeURIComponent(message.text),
+    language: locale,
+    //eslint-disable-next-line
+    device_type: 'Web Client',
+  });
+}
+
+export function getSusiPreviewReply(message) {
+  const url = `${API_URL}/${CHAT_API_PREFIX}/chat.json`;
+  return ajax.get(url, { q: message });
+}
+
+export function getBlogReponse() {
+  const url = 'https://api.rss2json.com/v1/api.json';
+  return ajax.get(url, {
+    count: '50',
+    //eslint-disable-next-line
+    api_key: BLOG_KEY,
+    //eslint-disable-next-line
+    rss_url: 'http://blog.fossasia.org/tag/susi-ai/feed/',
+  });
+}
+
+export function getHistory() {
+  const url = `${API_URL}/${CHAT_API_PREFIX}/memory.json`;
+  return ajax.get(url, {});
+}
+
+export function getWebSearchResults(query) {
+  return ajax.get('https://api.duckduckgo.com', { format: 'json', q: query });
+}
+
+export function previewURLForImage() {
+  return ajax.get();
 }
