a690e91bcd9363a149b58be66878b877e87c1659
Shubham Gupta <shubhamg@outlook.com>
2019-02-05 09:49:13 +0530

    Cookie from store (#1977)

--- src/__tests__/components/NotFound/NotFound.js
+++ src/__tests__/components/NotFound/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import NotFound from '../../../components/NotFound/NotFound.react';
 import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<NotFound />', () => {
   it('renders NotFound without crashing', () => {
-    shallow(<NotFound location={{ pathname: '' }} />);
+    shallow(
+      <Provider store={store}>
+        <NotFound location={{ pathname: '' }} />
+      </Provider>,
+    );
   });
 });
