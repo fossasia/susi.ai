31c4bd433d8d5b57f5cbc8ae0952ea38287f980e
Akshat Garg <akshatnitd@gmail.com>
2019-06-02 20:56:44 +0530

    Fixes #2163: Fix delete device modal open by default

--- src/__tests__/components/ChatApp/Settings/PasswordTab.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PasswordTab from '../../../../components/ChatApp/Settings/PasswordTab.react';
-import { shallow } from 'enzyme';
-
-describe('<PasswordTab />', () => {
-  it('render PasswordTab without crashing', () => {
-    shallow(<PasswordTab />);
-  });
-});
