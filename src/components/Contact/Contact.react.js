29217894c0d22c62a5c21bd23260796e6a896f95
Harsh <29hj29@gmail.com>
2019-06-25 14:14:37 +0530

    Integrate Styled components in Contact and Terms Page

--- src/components/Contact/Contact.react.js
+++ src/components/Contact/Contact.react.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { HeadSection } from '../shared/About';
+import { Header } from '../shared/About';
 import { scrollToTopAnimation } from '../../utils/animateScroll';
-import './Contact.css';
+import styled, { css } from 'styled-components';
+
+const commonContactContent = css`
+  padding: 3.125rem 0 1.563rem 0;
+  max-width: 100%;
+  font-family: sans-serif;
+  font-weight: 300;
+  text-align: left;
+  width: 100%;
+`;
+
+const ContactContent = styled.div`
+  ${commonContactContent};
+  border-bottom: 0.063rem solid #eee;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const Section = styled.div`
+  max-width: 100%;
+  width: 60%;
+  margin: 0 auto;
+  padding: 6.25rem 1.25rem 6.25rem 1.25rem;
+
+  @media (max-width: 1000px) {
+    padding: 4.375rem 1.25rem 3.125rem 1.25rem;
+  }
+
+  @media (max-width: 769px) {
+    width: 80%;
+  }
+`;
+
+const H5 = styled.h5`
+  font-size: 1.125rem;
+  font-weight: 400;
+`;
+
+const Text = styled.p`
+  display: block;
+  width: 85%;
+  line-height: 1.375rem;
+  font-weight: 500;
+`;
 
 const Contact = props => {
   document.title =
@@ -12,21 +57,15 @@ const Contact = props => {
 
   return (
     <div>
-      <HeadSection>
-        <div className="white-grey">
-          <div className="conversation__description">
-            <div className="support__heading">Contact Us</div>
-            <p className="support__text">
-              Get the help and information you need from our community and team
-              through various channels.
-            </p>
-          </div>
-        </div>
-      </HeadSection>
-      <div className="section">
-        <div className="contact-content">
-          <h5>SUSI</h5>
-          <p>
+      <Header
+        title="Contact Us"
+        subtitle="Get the help and information you need from our community and team
+              through various channels."
+      />
+      <Section>
+        <ContactContent>
+          <H5>SUSI</H5>
+          <Text>
             6 Eu Tong Sen Street
             <br />
             #5-07 The Central,
@@ -39,17 +78,17 @@ const Contact = props => {
             <br />
             Board of Directors: Phuc Hong Dang
             <br />
-          </p>
-        </div>
-        <div className="contact-content-last">
+          </Text>
+        </ContactContent>
+        <ContactContent>
           Report a safety or abuse issue affecting our products.
           <br />
           If you know of a safety or abuse problem with any of SUSI{"'"} s
           services{','}
           we{"'"}d like to hear about it right away. Please use our contact form
           to report the issue
-        </div>
-      </div>
+        </ContactContent>
+      </Section>
     </div>
   );
 };
