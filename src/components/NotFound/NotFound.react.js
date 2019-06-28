76b24f85059302b71edad3000f0d3d5b3c1d12f1
Shubham Gupta <shubhamg@outlook.com>
2019-06-28 10:31:42 +0530

    Fix logo on Page not Found #2348 (#2389)

--- src/components/NotFound/NotFound.react.js
+++ src/components/NotFound/NotFound.react.js
@@ -7,55 +7,59 @@ import uiActions from '../../redux/actions/ui';
 import Button from '@material-ui/core/Button';
 import './NotFound.css';
 import LogoImg from '../../images/susi-logo.svg';
+import styled from 'styled-components';
+
+const SusiLogo = styled.img.attrs({
+  alt: 'Page Not Found',
+  src: LogoImg,
+})`
+  width: 11rem;
+`;
 
 const NotFound = ({ accessToken, actions }) => {
   return (
-    <div>
-      <div className="container-fluid not-found-banner">
-        <h2>
-          <div className="susilogo">
-            <img src={LogoImg} to={'/'} alt="Page Not Found" />
-          </div>
-        </h2>
-        <h1>404</h1>
-        <h2>Page not found</h2>
-        <div className="button-wrapper">
-          <Link
-            style={{ textDecoration: 'none' }}
-            to={'/'}
-            className="actionButton"
+    <div className="not-found-banner">
+      <Link to={'/'}>
+        <SusiLogo />
+      </Link>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <div className="button-wrapper">
+        <Link
+          style={{ textDecoration: 'none' }}
+          to={'/'}
+          className="actionButton"
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            className="notfound-button"
           >
+            Chat With SUSI
+          </Button>
+        </Link>
+        <br />
+        {!accessToken && (
+          <div>
             <Button
+              className="notfound-button"
+              onClick={() => actions.openModal({ modalType: 'signUp' })}
               variant="contained"
               color="primary"
-              className="notfound-button"
             >
-              Chat With SUSI
+              SignUp to SUSI
             </Button>
-          </Link>
-          <br />
-          {!accessToken && (
-            <div>
-              <Button
-                className="notfound-button"
-                onClick={() => actions.openModal({ modalType: 'signUp' })}
-                variant="contained"
-                color="primary"
-              >
-                SignUp to SUSI
-              </Button>
-              <br />
-              <Button
-                className="notfound-button"
-                variant="contained"
-                color="primary"
-                onClick={() => actions.openModal({ modalType: 'login' })}
-              >
-                Login to SUSI
-              </Button>
-            </div>
-          )}
-        </div>
+            <br />
+            <Button
+              className="notfound-button"
+              variant="contained"
+              color="primary"
+              onClick={() => actions.openModal({ modalType: 'login' })}
+            >
+              Login to SUSI
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
