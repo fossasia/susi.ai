e7f6543575c82c57e8ab5020045cf26fefc05470
Shubham Gupta <shubhamg@outlook.com>
2019-06-30 02:13:20 +0530

    Remove TopBar, reuse StaticAppBar #2373

---
+++ src/components/NavigationBar/Styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import IconButton from '@material-ui/core/IconButton';
+
+export const StyledIconButton = styled(IconButton)`
+  border-radius: 2px;
+  padding: 0.5rem;
+`;
