989b02e845d941a5c2bfae067700262036c6df2a
Akshat Garg <akshatnitd@gmail.com>
2019-07-04 12:01:41 +0530

    Fixes #2437: Use .env for API_URL

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,7 @@
 const urls = {
-  API_URL: 'https://api.susi.ai',
+  API_URL: process.env.REACT_APP_API_URL || 'https://api.susi.ai',
   CHAT_URL: 'https://susi.ai/chat',
   SKILL_URL: 'https://susi.ai/skills',
-  ACCOUNT_URL: 'https://accounts.susi.ai',
   GITHUB_URL: 'https://github.com/fossasia/susi.ai',
   GITHUB_AVATAR_URL: 'https://avatars.githubusercontent.com',
   GRAVATAR_URL: 'https://www.gravatar.com/avatar',
