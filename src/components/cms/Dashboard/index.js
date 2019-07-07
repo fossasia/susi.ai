d051d19d3c23e51985f29ea6c6bb2f2d2809247c
Akshat Garg <akshatnitd@gmail.com>
2019-07-08 00:21:17 +0530

    Fixes #2439: Create Tabs on Dashboard page (#2486)

---
+++ src/components/cms/Dashboard/index.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import AppBar from '@material-ui/core/AppBar';
+import styled from 'styled-components';
+import DashboardContent from './Dashboard';
+import MySkills from '../SkillCreator/SkillCreator';
+import Botbuilder from '../BotBuilder/BotBuilder';
+import isMobileView from '../../../utils/isMobileView';
+import { templates } from '../BotBuilder/BotBuilderWrap';
+
+const Container = styled.div`
+  margin-top: 70px;
+  padding-left: 25px;
+  padding-right: 30px;
+`;
+
+const Tabs = styled(_Tabs)`
+  background-color: #ffffff;
+`;
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.getCurrentTab(),
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.setState({ value: this.getCurrentTab() });
+    }
+  }
+
+  getCurrentTab = () => {
+    switch (this.props.location.pathname) {
+      case '/skills/dashboard':
+        return 0;
+      case '/skills/myskills':
+        return 1;
+      case '/skills/botbuilder':
+        return 2;
+      default:
+        return 0;
+    }
+  };
+
+  handleTabChange = (event, value) => {
+    this.setState({ value });
+    const { history } = this.props;
+    switch (value) {
+      case 0:
+        history.replace('/skills/dashboard');
+        break;
+      case 1:
+        history.replace('/skills/myskills');
+        break;
+      case 2:
+        history.replace('/skills/botbuilder');
+        break;
+      default:
+        history.replace('/dashboard');
+    }
+  };
+
+  generateView = () => {
+    const { value } = this.state;
+    switch (value) {
+      case 0:
+        return <DashboardContent showTitle={false} />;
+      case 1:
+        return <MySkills showTitle={false} />;
+      case 2:
+        return <Botbuilder templates={templates} />;
+      default:
+        return;
+    }
+  };
+
+  render() {
+    const { value } = this.state;
+    const mobileView = isMobileView();
+    return (
+      <div>
+        <div>
+          <Container>
+            <AppBar color="default" position="static">
+              <Tabs
+                onChange={this.handleTabChange}
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                scrollButtons="on"
+                variant={mobileView ? 'scrollable' : 'standard'}
+              >
+                <Tab label="Dashboard" />
+                <Tab label="My Skills" />
+                <Tab label="Botbuilder" />
+              </Tabs>
+              {this.generateView()}
+            </AppBar>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  history: PropTypes.object,
+  location: PropTypes.object,
+};
+
+export default Dashboard;
