10e8c24f83be393ea3994b84fd1ab276793e00e0
samagragupta <samagraguptajpr@gmail.com>
2019-07-07 09:03:18 +0530

    Substitute dropdown menu by "Login" and "Name with dropdown arrow" when logged in

--- src/components/NavigationBar/Styles.js
+++ src/components/NavigationBar/Styles.js
@@ -3,5 +3,5 @@ import IconButton from '@material-ui/core/IconButton';
 
 export const StyledIconButton = styled(IconButton)`
   border-radius: 2px;
-  padding: 0.5rem;
+  padding: 0.2rem 0.5rem;
 `;
