12ff8b52f44763c993d1897f66291661c70487c4
Akshat Garg <akshatnitd@gmail.com>
2019-07-11 20:41:59 +0530

    Fixes #2495: Add "local pages" for smart devices to build script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+LOCAL_TARGET_BRANCH="local-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy.
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -70,4 +71,46 @@ else
 
   # Actual push to gh-pages branch via Travis
   git push --force $SSH_REPO $TARGET_BRANCH
+fi
+
+# Deploying to local-pages
+git checkout $LOCAL_TARGET_BRANCH || git checkout --orphan $LOCAL_TARGET_BRANCH
+cd ..
+
+# Cleaning up the old repo's gh-pages branch except CNAME file and 404.html
+find repo/* ! -name "CNAME" ! -name "404.html" -maxdepth 1  -exec rm -rf {} \; 2> /dev/null
+cd repo
+
+git add --all
+git commit -m "Travis CI Clean Deploy : ${SHA}"
+
+git checkout $SOURCE_BRANCH
+
+# Actual building and setup of current push or PR.
+npm install
+export REACT_APP_API_URL=http://localhost:4000
+npm run build
+mv build ../build/
+
+git checkout $LOCAL_TARGET_BRANCH
+rm -rf node_modules/
+mv ../build/* .
+cp index.html 404.html
+
+# Staging the new build for commit; and then committing the latest build
+git add -A
+git commit --amend --no-edit --allow-empty
+
+# Deploying only if the build has changed
+if [ -z `git diff --name-only HEAD HEAD~1` ]; then
+
+  echo "No Changes in the Build; exiting"
+  exit 0
+
+else
+  # There are changes in the Build; push the changes to gh-pages
+  echo "There are changes in the Build; pushing the changes to local-pages"
+
+  # Actual push to gh-pages branch via Travis
+  git push --force $SSH_REPO $LOCAL_TARGET_BRANCH
 fi
