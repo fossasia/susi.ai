ea05bde1741ca2cd953adeba0b35add045bcfe4f
Shubham Gupta <shubhamg@@outlook.com>
2019-07-14 03:53:29 +0530

    Integrate Styled-component in Not Found #2377

--- src/components/NotFound/NotFound.react.js
+++ src/components/NotFound/NotFound.react.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import uiActions from '../../redux/actions/ui';
-import Button from '@material-ui/core/Button';
-import './NotFound.css';
+import _Button from '@material-ui/core/Button';
 import LogoImg from '../../images/susi-logo.svg';
 import styled from 'styled-components';
 
@@ -17,7 +16,46 @@ const SusiLogo = styled.img.attrs({
 `;
 
 const LoginText = styled.p`
-  font-size: 60px;
+  font-size: 3.75rem;
+  @media (max-width: 650px) {
+    font-size: 2.5rem;
+    margin-top: 0.5rem;
+  }
+`;
+
+const Button = styled(_Button)`
+  width: 300px;
+  margin-top: 0.7rem;
+  @media (max-width: 356px) {
+    width: 100%;
+    margin-top: 0.7rem;
+  }
+`;
+
+const Container = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  background-size: cover;
+  min-height: 74vh;
+  width: 100%;
+  position: relative;
+  top: 70px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 72px;
+    margin: 0px 0 23px 0;
+  }
+  h2 {
+    font-size: 22px;
+    letter-spacing: 10px;
+  }
+  h2 a img {
+    width: 180px;
+    margin: 75px 0 -5px 0;
+  }
 `;
 
 const RenderText = ({ renderLogin }) => {
@@ -38,49 +76,36 @@ RenderText.propTypes = {
 
 const NotFound = ({ accessToken, actions, renderLogin = false }) => {
   return (
-    <div className="not-found-banner">
+    <Container>
       <Link to={'/'}>
         <SusiLogo />
       </Link>
       <RenderText renderLogin={renderLogin} />
-      <div className="button-wrapper">
-        <Link
-          style={{ textDecoration: 'none' }}
-          to={'/'}
-          className="actionButton"
-        >
+      <Link style={{ textDecoration: 'none' }} to={'/'}>
+        <Button variant="contained" color="primary">
+          Chat With SUSI
+        </Button>
+      </Link>
+      {!accessToken && (
+        <div>
+          <Button
+            onClick={() => actions.openModal({ modalType: 'signUp' })}
+            variant="contained"
+            color="primary"
+          >
+            SignUp to SUSI
+          </Button>
+          <br />
           <Button
             variant="contained"
             color="primary"
-            className="notfound-button"
+            onClick={() => actions.openModal({ modalType: 'login' })}
           >
-            Chat With SUSI
+            Login to SUSI
           </Button>
-        </Link>
-        <br />
-        {!accessToken && (
-          <div>
-            <Button
-              className="notfound-button"
-              onClick={() => actions.openModal({ modalType: 'signUp' })}
-              variant="contained"
-              color="primary"
-            >
-              SignUp to SUSI
-            </Button>
-            <br />
-            <Button
-              className="notfound-button"
-              variant="contained"
-              color="primary"
-              onClick={() => actions.openModal({ modalType: 'login' })}
-            >
-              Login to SUSI
-            </Button>
-          </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </Container>
   );
 };
 
