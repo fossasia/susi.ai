06d636329f3620d0793f09ef078712e07d8c5bb2
HarshJindal <31013104+harsh-jindal@users.noreply.github.com>
2019-07-16 23:44:38 +0530

    Fixes #2586 (#2593)

--- src/components/shared/Lozenge.js
+++ src/components/shared/Lozenge.js
@@ -3,18 +3,24 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Box = styled.div`
-  width: 40px;
   height: 40px;
+  width: auto;
+  max-width: fit-content;
   background-color: ${props => props.color};
   border-radius: 4px;
   text-align: center;
   margin: 0 auto;
 `;
 
+const Text = styled.p`
+  color: #fff;
+  padding: 0px 10px;
+`;
+
 const Lozenge = ({ text, color }) => {
   return (
     <Box color={color}>
-      <p style={{ color: '#fff' }}>{text}</p>
+      <Text>{text}</Text>
     </Box>
   );
 };
