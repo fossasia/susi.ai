a78c6f34496aaa998fd22d0f09bd786c3bdd9be8
tanujvyas10 <48439116+tanujvyas10@users.noreply.github.com>
2019-07-26 09:34:03 +0530

    Update index.js

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -96,12 +96,11 @@ const SusiLogo = styled.img`
 `;
 
 const TopRightMenuContainer = styled.div`
-color:white;
-display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 1px;
-  margin-left:-30px;
+  margin-left: -30px;
 `;
 
 const SusiLogoContainer = styled.div`
