e97c02709cdce468fe45c94df221fb935cdd1aef
Shubham Gupta <shubhamg@outlook.com>
2019-08-03 03:18:59 +0530

    Fix

--- src/apis/index.js
+++ src/apis/index.js
@@ -749,11 +749,11 @@ export function fetchMailSettings(payload) {
 export function changeMailSettings(payload) {
   const url = `${API_URL}/${AUTH_API_PREFIX}/emailSettings.json`;
   const {
-    frontendUrl,
+    frontendUrl: frontend_url,
     type,
     name,
     email,
-    sendgridToken,
+    sendgridToken: sendgrid_token,
     trustselfsignedcerts,
     encryption,
     port,
@@ -762,29 +762,29 @@ export function changeMailSettings(payload) {
     smtpHost,
   } = payload;
   return ajax.get(url, {
-    frontendUrl,
+    frontend_url,
     type,
     name,
     email,
-    sendgridToken,
+    sendgrid_token,
     trustselfsignedcerts,
     encryption,
     port,
     username,
     password,
-    smtpHost,
+    smtp_host: smtpHost,
   });
 }
 
 export function sendEmail(payload) {
   const url = `${API_URL}/${AUTH_API_PREFIX}/emailSettings.json`;
   const {
-    frontendUrl,
+    frontendUrl: frontend_url,
     type,
     name,
     email,
-    sendgridToken,
-    receiverEmail,
+    sendgridToken: sendgrid_token,
+    receiverEmail: receiver_email,
     trustselfsignedcerts,
     encryption,
     port,
@@ -793,18 +793,18 @@ export function sendEmail(payload) {
     smtpHost,
   } = payload;
   return ajax.get(url, {
-    frontendUrl,
+    frontend_url,
     type,
     name,
     email,
-    sendgridToken,
-    receiverEmail,
+    sendgrid_token,
+    receiver_email,
     trustselfsignedcerts,
     encryption,
     port,
     username,
     password,
-    smtpHost,
+    smtp_host: smtpHost,
   });
 }
 
