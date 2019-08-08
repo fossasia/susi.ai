e5efdf64e061f29c0d7276f376cf9e8bb94fec78
Shubham Gupta <shubhamg@outlook.com>
2019-08-09 01:44:28 +0530

    Add reset password component #2800

---
+++ src/__tests__/components/Auth/ResetPassword/ResetPassword.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ResetPassword from '../../../../components/Auth/ResetPassword/index';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+describe('<ResetPassword />', () => {
+  it('render ResetPassword without crashing', () => {
+    shallow(
+      <Provider store={store}>
+        <ResetPassword />
+      </Provider>,
+    );
+  });
+});
