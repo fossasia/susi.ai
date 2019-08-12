6e88c77cccc680f89acc1f1b263095eba57e5a00
Harsh <29hj29@gmail.com>
2019-08-12 19:04:47 +0530

    Admin Dashboard: Device Tab - Add Device Added Date, Location Map and Edit Device

--- src/components/Admin/ListDevices/constants.js
+++ src/components/Admin/ListDevices/constants.js
@@ -3,35 +3,62 @@ export const DEVICE = [
     title: 'Device Name',
     field: 'deviceName',
     cellStyle: {
-      width: '20%',
+      width: '10%',
     },
   },
   {
     title: 'Mac Id',
     field: 'macId',
     cellStyle: {
-      width: '20%',
-    },
-  },
-  {
-    title: 'Email',
-    field: 'email',
-    cellStyle: {
-      width: '20%',
+      width: '10%',
     },
   },
   {
     title: 'Room',
     field: 'room',
     cellStyle: {
-      width: '20%',
+      width: '10%',
+    },
+  },
+  {
+    title: 'Email',
+    field: 'email',
+    cellStyle: {
+      width: '10%',
+    },
+  },
+  {
+    title: 'Date Added',
+    field: 'dateAdded',
+    cellStyle: {
+      width: '10%',
+    },
+    customSort: (a, b) =>
+      new Date(a.dateAdded === '-' ? 'Thu Jan 01 1970' : a.dateAdded) -
+      new Date(b.dateAdded === '-' ? 'Thu Jan 01 1970' : b.dateAdded),
+  },
+  {
+    title: 'Last Active',
+    field: 'lastActive',
+    cellStyle: {
+      width: '10%',
+    },
+    customSort: (a, b) =>
+      new Date(a.lastActive === '-' ? 'Thu Jan 01 1970' : a.lastActive) -
+      new Date(b.lastActive === '-' ? 'Thu Jan 01 1970' : b.lastActive),
+  },
+  {
+    title: 'Last Login IP',
+    field: 'lastLoginIP',
+    cellStyle: {
+      width: '10%',
     },
   },
   {
     title: 'Location',
     field: 'location',
     cellStyle: {
-      width: '20%',
+      width: '10%',
     },
   },
 ];
