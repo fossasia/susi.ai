849d4aec4e16c9d51c5cb6b72b67bfc32201b250
Shubham Gupta <shubhamg@outlook.com>
2019-08-14 15:48:33 +0530

    Add login in the setup page[Smart Speaker] #2824

--- src/components/smart-speaker/Setup.js
+++ src/components/smart-speaker/Setup.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import susilogo from './susilogo.png';
+import susilogo from './susilogo.svg';
 import OutlinedTextField from '../shared/OutlinedTextField';
 import Button from '@material-ui/core/Button';
+import Select from '../shared/Select';
 import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import _FormControlLabel from '@material-ui/core/FormControlLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import MenuItem from '@material-ui/core/MenuItem';
 import { setupDeviceConfig } from '../../apis';
+import { isEmail } from '../../utils';
 
 const Container = styled.div`
   background: #f5f5f5;
@@ -16,7 +17,7 @@ const Container = styled.div`
   justify-content: center;
   height: 100vh;
   padding: 1rem;
-  margin-top: -50px;
+  margin-top: -30px;
 `;
 
 const Wrapper = styled.div`
@@ -28,15 +29,18 @@ const Wrapper = styled.div`
 `;
 
 const SusiLogo = styled.img`
-  width: 15rem;
-  height: 15rem;
+  width: 10rem;
+  height: 10rem;
+  margin: -40px 0;
 `;
 
-const RadioFlexContainer = styled.div`
-  display: flex;
-  padding: 1rem 0;
-  justify-content: space-between;
-  width: 80%;
+const FormControlLabel = styled(_FormControlLabel)`
+  align-self: flex-start;
+`;
+
+const P = styled.p`
+  font-size: 0.8rem;
+  margin-top: 0rem;
 `;
 
 class ControlPage extends React.Component {
@@ -44,16 +48,32 @@ class ControlPage extends React.Component {
     ssid: '',
     password: '',
     hotword: 'y',
-    wake: 'y',
+    email: '',
+    authPassword: '',
+    emailErrorText: '',
+    passwordErrorText: '',
+    authType: 'authenticated',
+    roomName: 'My room',
   };
 
   handleRebootSpeaker = () => {
-    const { ssid, password, hotword, wake } = this.state;
+    const {
+      ssid,
+      password,
+      hotword,
+      email,
+      authPassword,
+      authType: auth,
+      roomName,
+    } = this.state;
     setupDeviceConfig({
       wifissid: ssid,
       wifipassd: password,
       hotword,
-      wake,
+      email: auth === 'anonymous' ? '' : email,
+      password: auth === 'anonymous' ? '' : authPassword,
+      auth,
+      roomName,
     }).then(() => {
       window.location.reload();
     });
@@ -61,23 +81,37 @@ class ControlPage extends React.Component {
 
   handleInputFieldChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleRadioChange = e => {
-    const { name, value } = e.target;
+    if (name === 'email') {
+      isEmail(value)
+        ? this.setState({ emailErrorText: '' })
+        : this.setState({ emailErrorText: 'Invalid Email address' });
+    } else if (name === 'authPassword') {
+      value.length > 6
+        ? this.setState({ passwordErrorText: '' })
+        : this.setState({
+            passwordErrorText: 'Password should be atleast 6 characters',
+          });
+    }
     this.setState({ [name]: value });
   };
 
   render() {
-    const { ssid, password, hotword, wake } = this.state;
+    const {
+      ssid,
+      password,
+      hotword,
+      email,
+      authPassword,
+      emailErrorText,
+      passwordErrorText,
+      authType,
+      roomName,
+    } = this.state;
     return (
       <Container>
         <Wrapper>
           <SusiLogo src={susilogo} />
-          <div>
-            <h1>Setup your WiFi for SUSI.AI</h1>
-          </div>
+          <h2>Setup the WiFi for Your Smart Speaker</h2>
           <OutlinedTextField
             fullWidth
             label="SSID"
@@ -86,65 +120,125 @@ class ControlPage extends React.Component {
             name="ssid"
             onChange={this.handleInputFieldChange}
             style={{ background: 'white' }}
+            margin="dense"
           />
           <OutlinedTextField
             fullWidth
-            label="Password"
-            placeholder="Password"
+            label="WiFi Password"
+            placeholder="WiFi Password"
             value={password}
             name="password"
             onChange={this.handleInputFieldChange}
             style={{ background: 'white' }}
+            margin="dense"
           />
-          <RadioFlexContainer>
-            <div>
-              <FormControl>
-                <FormLabel component="legend">Hotword</FormLabel>
-                <RadioGroup
-                  aria-label="Hotword"
-                  name="hotword"
-                  value={hotword}
-                  onChange={this.handleRadioChange}
-                >
-                  <FormControlLabel
-                    value="y"
-                    control={<Radio />}
-                    label="Snowboy"
-                  />
-                  <FormControlLabel
-                    value="n"
-                    control={<Radio />}
-                    label="Pocket Sphinx"
-                  />
-                </RadioGroup>
-              </FormControl>
-            </div>
-            <div>
-              <FormControl>
-                <FormLabel component="legend">Wake Button</FormLabel>
-                <RadioGroup
-                  aria-label="wake"
-                  name="wake"
-                  value={wake}
-                  onChange={this.handleRadioChange}
-                >
-                  <FormControlLabel value="y" control={<Radio />} label="On" />
-                  <FormControlLabel value="n" control={<Radio />} label="Off" />
-                </RadioGroup>
-              </FormControl>
-            </div>
-          </RadioFlexContainer>
-
-          <p>
-            To reset your SUSI.AI Smart Speaker (to get back here), hold the
-            button for 10 seconds.
-          </p>
+          <OutlinedTextField
+            fullWidth
+            label="Room name"
+            placeholder="Room name"
+            value={roomName}
+            name="roomName"
+            onChange={this.handleInputFieldChange}
+            style={{ background: 'white' }}
+            margin="dense"
+          />
+          <h2>Choose Hotword Recognition</h2>
+          <Select
+            value={hotword}
+            fullWidth
+            onChange={this.handleInputFieldChange}
+            style={{ background: 'white', marginTop: 8 }}
+            input={<OutlinedInput name="hotword" id="hotword" margin="dense" />}
+          >
+            <MenuItem value="y">Snowboy</MenuItem>
+            <MenuItem value="n">Pocket Sphinx</MenuItem>
+          </Select>
+          <h2>Add Smart Speaker to Your SUSI.AI Account</h2>
+          <P>
+            This will allow you to use private skills you created online and
+            synchronize your data. You cannot do this later. If you don&#39;t
+            have an account on SUSI.AI, please connect to the Internet sign and
+            up at SUSI.AI now.
+          </P>
+          <FormControlLabel
+            value={authType}
+            name="authType"
+            control={
+              <Radio
+                color="primary"
+                checked={authType === 'authenticated'}
+                value="authenticated"
+              />
+            }
+            label={<p>Connect to SUSI.AI</p>}
+            labelPlacement="end"
+            onChange={this.handleInputFieldChange}
+          />
+          {authType === 'authenticated' && (
+            <React.Fragment>
+              <OutlinedTextField
+                fullWidth
+                label="Email"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={this.handleInputFieldChange}
+                style={{ background: 'white' }}
+                aria-describedby="email"
+                margin="dense"
+                disabled={authType === 'anonymous'}
+                error={emailErrorText !== ''}
+              />
+              <OutlinedTextField
+                fullWidth
+                label={'Password'}
+                placeholder="Password"
+                value={authPassword}
+                name="authPassword"
+                onChange={this.handleInputFieldChange}
+                style={{ background: 'white' }}
+                margin="dense"
+                disabled={authType === 'anonymous'}
+                error={passwordErrorText !== ''}
+              />
+            </React.Fragment>
+          )}
+          <FormControlLabel
+            value={authType}
+            name="authType"
+            control={
+              <Radio
+                color="primary"
+                checked={authType === 'anonymous'}
+                value="anonymous"
+              />
+            }
+            label={<p>Anonymous Mode</p>}
+            labelPlacement="end"
+            onChange={this.handleInputFieldChange}
+          />
+          <br />
+          <P>
+            Thank you for entering your details. You can now save your data.
+            After you hit the &#34;Save&#34; button the device will shut down
+            and reboot. The smart speaker will connect with your WiFi network
+            and add the device to your SUSI.AI account, if you provided your
+            credentials. If the smart speaker is not able to connect with the
+            WiFi it will restart and open a hotspot again. You can configure the
+            device again.
+          </P>
+          <P>
+            If you would like to reset your device later manually (to get back
+            here) you can press the device button for 10 seconds. The device
+            will reset and you can access it again through the hotspot and
+            configure it.
+          </P>
           <Button
             variant="contained"
             color="primary"
             onClick={this.handleRebootSpeaker}
           >
-            Reboot Smart Speaker
+            Save and Reboot
           </Button>
         </Wrapper>
       </Container>
