519d86bd55b9eeb6a65032b8c12310f392ba073c
Mario Behling <mb@mariobehling.de>
2019-10-28 13:46:10 +0100

    added instruments

--- conf.py
+++ conf.py
@@ -58,4 +58,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-master_doc = 'introduction'
+master_doc = 'index'
