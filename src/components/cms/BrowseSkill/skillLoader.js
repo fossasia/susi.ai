1621a28c8e77dacfa617421e24c245784a299531
kpulkit291 <pulkit.kashyap@magicpin.in>
2019-11-30 19:29:17 +0530

    fix(): loader to match the current ui

--- src/components/cms/BrowseSkill/skillLoader.js
+++ src/components/cms/BrowseSkill/skillLoader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Facebook } from 'react-content-loader';
+import _ContentLoader from 'react-content-loader';
 import styled from 'styled-components';
 import isMobileView from '../../../utils/isMobileView';
 
@@ -7,8 +7,8 @@ const Container = styled.div`
   display: flex;
 `;
 
-const Loader = styled(Facebook)`
-  width: 260px;
+const ContentLoader = styled(_ContentLoader)`
+  width: 280px;
   height: 168px;
   margin: 20px;
 `;
@@ -17,7 +17,15 @@ const addLoader = () => {
   const loader = [];
   const limit = isMobileView() ? 2 : 4;
   for (let i = 0; i < limit; i++) {
-    loader.push(<Loader />);
+    loader.push(
+      <ContentLoader>
+        <circle cx="34" cy="34" r="34" />
+        <rect x="75" y="18" rx="4" ry="4" width="140" height="30" />
+        <rect x="0" y="100" rx="5" ry="5" width="80" height="14" />
+        <rect x="0" y="120" rx="5" ry="5" width="140" height="12" />
+        <rect x="150" y="120" rx="5" ry="5" width="40" height="12" />
+      </ContentLoader>,
+    );
   }
   return loader;
 };
