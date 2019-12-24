3f093dc23c54026b175cb043ff7f9ad9c3ad9ccb
kpulkit291 <pulkit.kashyap@magicpin.in>
2019-12-24 23:39:45 +0530

    chore(loader): added border and adjusted ratio

--- src/components/cms/BrowseSkill/skillLoader.js
+++ src/components/cms/BrowseSkill/skillLoader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import _ContentLoader from 'react-content-loader';
+import ContentLoader from 'react-content-loader';
 import styled from 'styled-components';
 import isMobileView from '../../../utils/isMobileView';
 
@@ -7,10 +7,11 @@ const Container = styled.div`
   display: flex;
 `;
 
-const ContentLoader = styled(_ContentLoader)`
+const ContentContainer = styled.div`
   width: 260px;
   height: 168px;
-  margin: 20px;
+  margin: 8px;
+  border: 0.5px solid #eaeded;
 `;
 
 const addLoader = () => {
@@ -18,12 +19,14 @@ const addLoader = () => {
   const limit = isMobileView() ? 2 : 4;
   for (let i = 0; i < limit; i++) {
     loader.push(
-      <ContentLoader speed={2}>
-        <circle cx="28" cy="28" r="28" />
-        <rect x="75" y="14" rx="4" ry="4" width="140" height="30" />
-        <rect x="0" y="80" rx="8" ry="8" width="200" height="8" />
-        <rect x="0" y="100" rx="8" ry="8" width="120" height="8" />
-      </ContentLoader>,
+      <ContentContainer>
+        <ContentLoader width={260} height={168} speed={2}>
+          <circle cx="44" cy="46" r="28" />
+          <rect x="100" y="24" rx="4" ry="4" width="120" height="44" />
+          <rect x="10" y="120" rx="8" ry="8" width="180" height="10" />
+          <rect x="10" y="140" rx="8" ry="8" width="120" height="10" />
+        </ContentLoader>
+      </ContentContainer>,
     );
   }
   return loader;
