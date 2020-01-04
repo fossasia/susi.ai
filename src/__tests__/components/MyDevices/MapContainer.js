90e2c6c01d55f90821248968f9f5a63e65ddcddf
anishagg17 <43617894+anishagg17@users.noreply.github.com>
2020-01-04 21:39:59 +0530

    added Map to contact page (#3178)
    Co-authored-by: Akshat Garg <akshatnitd@gmail.com>

--- src/__tests__/components/MyDevices/MapContainer.js
+++ src/__tests__/components/MyDevices/MapContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import MapContainer from '../../../components/cms/MyDevices/MapContainer';
 import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<MapContainer />', () => {
   it('renders MapContainer without crashing', () => {
-    shallow(<MapContainer />);
+    shallow(
+      <Provider store={store}>
+        <MapContainer />
+      </Provider>,
+    );
   });
 });
