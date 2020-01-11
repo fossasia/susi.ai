f1f243f0a6c51ad64f4e64b71f7237e0a74a7a15
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-01-11 17:48:22 +0530

    Fixed feedback form

--- src/__tests__/components/SkillFeedbackPage/EditFeedbackDialog.js
+++ src/__tests__/components/SkillFeedbackPage/EditFeedbackDialog.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import EditFeedback from '../../../components/cms/SkillFeedbackPage/EditFeedbackDialog';
 import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<EditFeedback />', () => {
   it('render EditFeedback without crashing', () => {
-    shallow(<EditFeedback />);
+    shallow(
+      <Provider store={store}>
+        <EditFeedback />
+      </Provider>,
+    );
   });
 });
