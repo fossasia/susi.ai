6d524ac9f157b563b241600a25602daf5868aaaa
A.Dilshaad <dilshaadmuthalif21@gmail.com>
2020-02-13 11:30:34 +0530

    Integrated rating and feedback (#3442)

---
+++ src/components/shared/FiveStarRatingWidget.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Ratings from 'react-ratings-declarative';
+import PropTypes from 'prop-types';
+
+const FiveStarRatingWidget = props => {
+  const { rating, ...otherProps } = props;
+  return (
+    <Ratings rating={rating} {...otherProps}>
+      <Ratings.Widget />
+      <Ratings.Widget />
+      <Ratings.Widget />
+      <Ratings.Widget />
+      <Ratings.Widget />
+    </Ratings>
+  );
+};
+
+FiveStarRatingWidget.propTypes = {
+  widgetRatedColors: PropTypes.string,
+  widgetDimensions: PropTypes.string,
+  widgetSpacings: PropTypes.string,
+  rating: PropTypes.number,
+};
+
+FiveStarRatingWidget.defaultProps = {
+  widgetRatedColors: '#ffbb28',
+  widgetDimensions: '20px',
+  widgetSpacings: '0px',
+};
+
+export default FiveStarRatingWidget;
