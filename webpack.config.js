e7d420f95853a62811c23e09fb13431c1cbaee0c
Ashwani Gupta <ashwanigupta125@gmail.com>
2020-05-24 01:51:11 +0530

    revert port number change

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = ({ mode }) => {
       devServer: {
         historyApiFallback: true,
         contentBase: path.join(__dirname, 'public'),
-        port: 4000,
+        port: 3000,
         hot: true,
       },
       entry: {
